b787862df0564201bef57fa7f995aa678bd76594
blais <unknown>
2008-06-07 10:51:21 -0700

    misc

---
+++ examples/demo1.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# This command runs the beancount web server on a demo ledger file.
+# Run this in a shell and use a web browser to access http://localhost:8000
+# The demo file contains many example transactions.
+ROOT=..
+export PYTHONPATH=$ROOT/lib/python:$ROOT/lib/python-fallback:$PYTHONPATH
+python $ROOT/bin/bean-serve --debug demo.ledger
--- examples/demo2.sh
+++ examples/demo2.sh
@@ -2,5 +2,6 @@
 # This command runs the beancount web server on a demo ledger file.
 # Run this in a shell and use a web browser to access http://localhost:8000
 # The demo file contains many example transactions.
-export PYTHONPATH=$PWD/lib/python:$PWD/lib/python-fallback:$PYTHONPATH
-python bin/bean-serve --debug examples/demo.ledger
+ROOT=..
+export PYTHONPATH=$ROOT/lib/python:$ROOT/lib/python-fallback:$PYTHONPATH
+python $ROOT/bin/bean-serve -g SKii-trip demo.ledger
