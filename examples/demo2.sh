36f2d4ea6d77362dfc893708ee1918df09b4ed80
blais <unknown>
2008-06-07 12:23:01 -0700

    removed references to python-fallback

--- examples/demo2.sh
+++ examples/demo2.sh
@@ -2,6 +2,5 @@
 # This command runs the beancount web server on a demo ledger file.
 # Run this in a shell and use a web browser to access http://localhost:8000
 # The demo file contains many example transactions.
-ROOT=..
-export PYTHONPATH=$ROOT/lib/python:$ROOT/lib/python-fallback:$PYTHONPATH
-python $ROOT/bin/bean-serve -g SKii-trip demo.ledger
+PROJDIR=.. . ../etc/env
+python $PROJDIR/bin/bean-serve -g SKii-trip demo.ledger
