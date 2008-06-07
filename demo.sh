d3de1504caefc5d0ff4f76f0fa1436d34e8cd96c
blais <unknown>
2008-06-07 09:27:09 -0700

    More details on demo ledger

--- demo.sh
+++ demo.sh
@@ -2,4 +2,5 @@
 # This command runs the beancount web server on a demo ledger file.
 # Run this in a shell and use a web browser to access http://localhost:8000
 # The demo file contains many example transactions.
+export PYTHONPATH=$PWD/lib/python:$PWD/lib/python-fallback:$PYTHONPATH
 python bin/bean-serve --debug examples/demo.ledger
