0fb9804cb4abde4e4cdc105e19a8549d89245b7c
blais <unknown>
2008-07-18 19:23:58 +0100

    misc

--- lib/python/beancount/cmdline.py
+++ lib/python/beancount/cmdline.py
@@ -141,8 +141,6 @@ def run_postprocesses(ledger, opts):
         pred = create_filter_pred(opts)
         ledger.filter_postings(pred)
     
-    if opts.reduce:
-        ledger.reduce(opts.reduce)
 
 
 
@@ -157,10 +155,6 @@ def addopts(parser):
     parser.add_option('-c', '--close', '--close-books',
                       action='store', metavar='TIME_EXPR',
                       help="Close the books at the given time.")
-
-    parser.add_option('-r', '--reduce', action='store',
-                      help="Reduce all amounts to a single commodity, using a rate "
-                      "calculated so it will neutralize the trial balance to nothing.")
 
     group = optparse.OptionGroup(parser, "Options for filtering postings.")
 
