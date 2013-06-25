3d3abc96a230f4cc594139624876393dda43ae01
Martin Blais <blais@furius.ca>
2013-06-25 00:19:31 -0400

    Farmed out routines for rendering HTML tables of entries.

---
+++ src/python/beancount/web/journal.py
@@ -0,0 +1,272 @@
+"""HTML rendering routines for serving a lists of postings/entries.
+"""
+from os import path
+
+from bottle import request
+
+from beancount.core import data
+from beancount.core.data import Open, Close, Check, Transaction, Note, Document
+from beancount.core.balance import get_balance_amount
+from beancount.core.inventory import Inventory
+from beancount.core.account import Account, account_leaf_name
+from beancount.core.realization import RealAccount
+from beancount.core import realization
+from beancount.core import flags
+
+
+_account_link_cache = {}
+
+def account_link(account_name, leafonly=False):
+    "Render an anchor for the given account name."
+    if isinstance(account_name, (Account, RealAccount)):
+        account_name = account_name.name
+    try:
+        return _account_link_cache[(request.app, account_name)]
+    except KeyError:
+        slashed_name = account_name.replace(':', '/')
+
+        if leafonly:
+            account_name = account_leaf_name(account_name)
+
+        link = '<a href="{}" class="account">{}</a>'.format(
+            request.app.get_url('account', slashed_account_name=slashed_name),
+            account_name)
+        _account_link_cache[account_name] = link
+        return link
+
+
+FLAG_ROWTYPES = {
+    flags.FLAG_PADDING  : 'Padding',
+    flags.FLAG_SUMMARIZE: 'Summarize',
+    flags.FLAG_TRANSFER : 'Transfer',
+}
+
+def balance_html(balance):
+    return ('<br/>'.join(map(str, balance.get_positions()))
+            if balance
+            else '')
+
+def entries_table_with_balance(app, oss, account_postings, render_postings=True):
+    """Render a list of entries into an HTML table.
+    """
+    write = lambda data: (oss.write(data), oss.write('\n'))
+
+    write('''
+      <table class="entry-table">
+      <thead>
+        <tr>
+         <th class="datecell">Date</th>
+         <th class="flag">F</th>
+         <th class="description">Narration/Payee</th>
+         <th class="position">Position</th>
+         <th class="price">Price</th>
+         <th class="cost">Cost</th>
+         <th class="change">Change</th>
+         <th class="balance">Balance</th>
+      </thead>
+    ''')
+
+    balance = Inventory()
+    for entry, leg_postings, change, balance in realization.iterate_with_balance(account_postings):
+
+        # Prepare the data to be rendered for this row.
+        date = entry.date
+        balance_str = balance_html(balance)
+
+        rowtype = entry.__class__.__name__
+        flag = ''
+        extra_class = ''
+
+        if isinstance(entry, Transaction):
+            rowtype = FLAG_ROWTYPES.get(entry.flag, 'Transaction')
+            extra_class = 'warning' if entry.flag == flags.FLAG_WARNING else ''
+            flag = entry.flag
+            description = '<span class="narration">{}</span>'.format(entry.narration)
+            if entry.payee:
+                description = '<span class="payee">{}</span><span class="pnsep">|</span>{}'.format(entry.payee, description)
+            change_str = balance_html(change)
+
+        elif isinstance(entry, Check):
+            # Check the balance here and possibly change the rowtype
+            if entry.errdiff is None:
+                description = 'Check {} has {}'.format(account_link(entry.account), entry.amount)
+            else:
+                description = 'Check in {} fails; expected = {}, balance = {}, difference = {}'.format(
+                    account_link(entry.account), entry.amount,
+                    balance.get_amount(entry.amount.currency),
+                    entry.errdiff)
+                rowtype = 'CheckFail'
+
+            change_str = str(entry.amount)
+
+        elif isinstance(entry, (Open, Close)):
+            description = '{} {}'.format(entry.__class__.__name__, account_link(entry.account))
+            change_str = ''
+
+        elif isinstance(entry, Note):
+            description = '{} {}'.format(entry.__class__.__name__, entry.comment)
+            change_str = ''
+            balance_str = ''
+
+        elif isinstance(entry, Document):
+            assert path.isabs(entry.filename)
+            description = 'Document for {}: "<a href="{}" class="filename">{}</a>"'.format(
+                account_link(entry.account),
+                app.router.build('doc', filename=entry.filename.lstrip('/')),
+                path.basename(entry.filename))
+            change_str = ''
+            balance_str = ''
+
+        else:
+            description = entry.__class__.__name__
+            change_str = ''
+            balance_str = ''
+
+        # Render a row.
+        write('''
+          <tr class="{} {}" title="{}">
+            <td class="datecell">{}</td>
+            <td class="flag">{}</td>
+            <td class="description" colspan="4">{}</td>
+            <td class="change num">{}</td>
+            <td class="balance num">{}</td>
+          <tr>
+        '''.format(rowtype, extra_class,
+                   '{}:{}'.format(entry.fileloc.filename, entry.fileloc.lineno),
+                   date, flag, description, change_str, balance_str))
+
+        if render_postings and isinstance(entry, Transaction):
+            for posting in entry.postings:
+
+                classes = ['Posting']
+                if posting.flag == flags.FLAG_WARNING:
+                    classes.append('warning')
+                if posting in leg_postings:
+                    classes.append('leg')
+
+                write('''
+                  <tr class="{}">
+                    <td class="datecell"></td>
+                    <td class="flag">{}</td>
+                    <td class="description">{}</td>
+                    <td class="position num">{}</td>
+                    <td class="price num">{}</td>
+                    <td class="cost num">{}</td>
+                    <td class="change num"></td>
+                    <td class="balance num"></td>
+                  <tr>
+                '''.format(' '.join(classes),
+                           posting.flag or '',
+                           account_link(posting.account),
+                           posting.position,
+                           posting.price or '',
+                           get_balance_amount(posting)))
+
+    write('</table>')
+
+
+def entries_table(app, oss, account_postings, render_postings=True):
+    """Render a list of entries into an HTML table.
+    """
+    write = lambda data: (oss.write(data), oss.write('\n'))
+
+    write('''
+      <table class="entry-table">
+      <thead>
+        <tr>
+         <th class="datecell">Date</th>
+         <th class="flag">F</th>
+         <th class="description">Narration/Payee</th>
+         <th class="amount">Amount</th>
+         <th class="cost">Cost</th>
+         <th class="price">Price</th>
+         <th class="balance">Balance</th>
+      </thead>
+    ''')
+
+    balance = Inventory()
+    for entry, leg_postings, change, balance in iterate_with_balance(account_postings):
+
+        # Prepare the data to be rendered for this row.
+        date = entry.date
+        rowtype = entry.__class__.__name__
+        flag = ''
+        extra_class = ''
+
+        if isinstance(entry, Transaction):
+            rowtype = FLAG_ROWTYPES.get(entry.flag, 'Transaction')
+            extra_class = 'warning' if entry.flag == flags.FLAG_WARNING else ''
+            flag = entry.flag
+            description = '<span class="narration">{}</span>'.format(entry.narration)
+            if entry.payee:
+                description = '<span class="payee">{}</span><span class="pnsep">|</span>{}'.format(entry.payee, description)
+            change_str = balance_html(change)
+
+        elif isinstance(entry, Check):
+            # Check the balance here and possibly change the rowtype
+            if entry.errdiff is None:
+                description = 'Check {} has {}'.format(account_link(entry.account), entry.amount)
+            else:
+                description = 'Check in {} fails; expected = {}, balance = {}, difference = {}'.format(
+                    account_link(entry.account), entry.amount,
+                    balance.get_amount(entry.amount.currency),
+                    entry.errdiff)
+                rowtype = 'CheckFail'
+
+        elif isinstance(entry, (Open, Close)):
+            description = '{} {}'.format(entry.__class__.__name__, account_link(entry.account))
+
+        elif isinstance(entry, Note):
+            description = '{} {}'.format(entry.__class__.__name__, entry.comment)
+            change_str = ''
+            balance_str = ''
+
+        elif isinstance(entry, Document):
+            assert path.isabs(entry.filename)
+            description = 'Document for {}: "<a href="{}" class="filename">{}</a>"'.format(
+                account_link(entry.account),
+                app.router.build('doc', filename=entry.filename.lstrip('/')),
+                path.basename(entry.filename))
+            change_str = ''
+            balance_str = ''
+
+        else:
+            description = entry.__class__.__name__
+
+        # Render a row.
+        write('''
+          <tr class="{} {}" title="{}">
+            <td class="datecell">{}</td>
+            <td class="flag">{}</td>
+            <td class="description" colspan="5">{}</td>
+          <tr>
+        '''.format(rowtype, extra_class,
+                   '{}:{}'.format(entry.fileloc.filename, entry.fileloc.lineno),
+                   date, flag, description))
+
+        if render_postings and isinstance(entry, Transaction):
+            for posting in entry.postings:
+
+                classes = ['Posting']
+                if posting.flag == flags.FLAG_WARNING:
+                    classes.append('warning')
+
+                write('''
+                  <tr class="{}">
+                    <td class="datecell"></td>
+                    <td class="flag">{}</td>
+                    <td class="description">{}</td>
+                    <td class="amount num">{}</td>
+                    <td class="cost num">{}</td>
+                    <td class="price num">{}</td>
+                    <td class="balance num">{}</td>
+                  <tr>
+                '''.format(' '.join(classes),
+                           posting.flag or '',
+                           account_link(posting.account),
+                           posting.position.get_amount(),
+                           posting.position.lot.cost or '',
+                           posting.price or '',
+                           get_balance_amount(posting)))
+
+    write('</table>')
--- src/python/beancount/web/web.py
+++ src/python/beancount/web/web.py
@@ -11,13 +11,14 @@ from bottle import response, request
 
 from beancount.web.bottle_utils import AttrMapper, internal_redirect
 from beancount.web import views
+from beancount.web import journal
+from beancount.web.journal import account_link
 from beancount.core import data
 from beancount.core.account import Account, account_leaf_name, is_account_root
 from beancount.core.position import Lot
-from beancount.core.data import Open, Close, Check, Transaction, Note, Document
+from beancount.core.data import Open, Close, Transaction, Document
 from beancount.core import getters
 from beancount.ops import summarize
-from beancount.core.balance import get_balance_amount
 from beancount.core.inventory import Inventory
 from beancount.core import realization
 from beancount.core.realization import RealAccount
@@ -26,7 +27,6 @@ from beancount import utils
 from beancount.utils.text_utils import replace_numbers
 from beancount.core.account import Account
 from beancount.core.account import is_balance_sheet_account_name
-from beancount.core import flags
 from beancount.loader import load
 
 
@@ -248,30 +248,12 @@ def approot():
 
 
 
-EMS_PER_SPACE = 2.5
-_account_link_cache = {}
-
-def account_link(account_name, leafonly=False):
-    "Render an anchor for the given account name."
-    if isinstance(account_name, (Account, RealAccount)):
-        account_name = account_name.name
-    try:
-        return _account_link_cache[(request.app, account_name)]
-    except KeyError:
-        slashed_name = account_name.replace(':', '/')
-
-        if leafonly:
-            account_name = account_leaf_name(account_name)
-
-        link = '<a href="{}" class="account">{}</a>'.format(
-            request.app.get_url('account', slashed_account_name=slashed_name),
-            account_name)
-        _account_link_cache[account_name] = link
-        return link
 
 
 # A special enum for the "Totals" line at the bottom of the table.
 TOTALS_LINE = object()
+
+EMS_PER_SPACE = 2.5
 
 def tree_table(oss, tree, start_node_name, header=None, classes=None):
     """Generator to a tree of accounts as an HTML table.
@@ -567,252 +549,8 @@ def equity():
                       contents = contents)
 
 
-
-
-
-
-
-
-
-FLAG_ROWTYPES = {
-    flags.FLAG_PADDING  : 'Padding',
-    flags.FLAG_SUMMARIZE: 'Summarize',
-    flags.FLAG_TRANSFER : 'Transfer',
-}
-
-def balance_html(balance):
-    return ('<br/>'.join(map(str, balance.get_positions()))
-            if balance
-            else '')
-
-def entries_table_with_balance(oss, account_postings, render_postings=True):
-    """Render a list of entries into an HTML table.
-    """
-    write = lambda data: (oss.write(data), oss.write('\n'))
-
-    write('''
-      <table class="entry-table">
-      <thead>
-        <tr>
-         <th class="datecell">Date</th>
-         <th class="flag">F</th>
-         <th class="description">Narration/Payee</th>
-         <th class="position">Position</th>
-         <th class="price">Price</th>
-         <th class="cost">Cost</th>
-         <th class="change">Change</th>
-         <th class="balance">Balance</th>
-      </thead>
-    ''')
-
-    balance = Inventory()
-    for entry, leg_postings, change, balance in realization.iterate_with_balance(account_postings):
-
-        # Prepare the data to be rendered for this row.
-        date = entry.date
-        balance_str = balance_html(balance)
-
-        rowtype = entry.__class__.__name__
-        flag = ''
-        extra_class = ''
-
-        if isinstance(entry, Transaction):
-            rowtype = FLAG_ROWTYPES.get(entry.flag, 'Transaction')
-            extra_class = 'warning' if entry.flag == flags.FLAG_WARNING else ''
-            flag = entry.flag
-            description = '<span class="narration">{}</span>'.format(entry.narration)
-            if entry.payee:
-                description = '<span class="payee">{}</span><span class="pnsep">|</span>{}'.format(entry.payee, description)
-            change_str = balance_html(change)
-
-        elif isinstance(entry, Check):
-            # Check the balance here and possibly change the rowtype
-            if entry.errdiff is None:
-                description = 'Check {} has {}'.format(account_link(entry.account), entry.amount)
-            else:
-                description = 'Check in {} fails; expected = {}, balance = {}, difference = {}'.format(
-                    account_link(entry.account), entry.amount,
-                    balance.get_amount(entry.amount.currency),
-                    entry.errdiff)
-                rowtype = 'CheckFail'
-
-            change_str = str(entry.amount)
-
-        elif isinstance(entry, (Open, Close)):
-            description = '{} {}'.format(entry.__class__.__name__, account_link(entry.account))
-            change_str = ''
-
-        elif isinstance(entry, Note):
-            description = '{} {}'.format(entry.__class__.__name__, entry.comment)
-            change_str = ''
-            balance_str = ''
-
-        elif isinstance(entry, Document):
-            assert path.isabs(entry.filename)
-            description = 'Document for {}: "<a href="{}" class="filename">{}</a>"'.format(
-                account_link(entry.account),
-                app.router.build('doc', filename=entry.filename.lstrip('/')),
-                path.basename(entry.filename))
-            change_str = ''
-            balance_str = ''
-
-        else:
-            description = entry.__class__.__name__
-            change_str = ''
-            balance_str = ''
-
-        # Render a row.
-        write('''
-          <tr class="{} {}" title="{}">
-            <td class="datecell">{}</td>
-            <td class="flag">{}</td>
-            <td class="description" colspan="4">{}</td>
-            <td class="change num">{}</td>
-            <td class="balance num">{}</td>
-          <tr>
-        '''.format(rowtype, extra_class,
-                   '{}:{}'.format(entry.fileloc.filename, entry.fileloc.lineno),
-                   date, flag, description, change_str, balance_str))
-
-        if render_postings and isinstance(entry, Transaction):
-            for posting in entry.postings:
-
-                classes = ['Posting']
-                if posting.flag == flags.FLAG_WARNING:
-                    classes.append('warning')
-                if posting in leg_postings:
-                    classes.append('leg')
-
-                write('''
-                  <tr class="{}">
-                    <td class="datecell"></td>
-                    <td class="flag">{}</td>
-                    <td class="description">{}</td>
-                    <td class="position num">{}</td>
-                    <td class="price num">{}</td>
-                    <td class="cost num">{}</td>
-                    <td class="change num"></td>
-                    <td class="balance num"></td>
-                  <tr>
-                '''.format(' '.join(classes),
-                           posting.flag or '',
-                           account_link(posting.account),
-                           posting.position,
-                           posting.price or '',
-                           get_balance_amount(posting)))
-
-    write('</table>')
-
-
-def entries_table(oss, account_postings, render_postings=True):
-    """Render a list of entries into an HTML table.
-    """
-    write = lambda data: (oss.write(data), oss.write('\n'))
-
-    write('''
-      <table class="entry-table">
-      <thead>
-        <tr>
-         <th class="datecell">Date</th>
-         <th class="flag">F</th>
-         <th class="description">Narration/Payee</th>
-         <th class="amount">Amount</th>
-         <th class="cost">Cost</th>
-         <th class="price">Price</th>
-         <th class="balance">Balance</th>
-      </thead>
-    ''')
-
-    balance = Inventory()
-    for entry, leg_postings, change, balance in iterate_with_balance(account_postings):
-
-        # Prepare the data to be rendered for this row.
-        date = entry.date
-        rowtype = entry.__class__.__name__
-        flag = ''
-        extra_class = ''
-
-        if isinstance(entry, Transaction):
-            rowtype = FLAG_ROWTYPES.get(entry.flag, 'Transaction')
-            extra_class = 'warning' if entry.flag == flags.FLAG_WARNING else ''
-            flag = entry.flag
-            description = '<span class="narration">{}</span>'.format(entry.narration)
-            if entry.payee:
-                description = '<span class="payee">{}</span><span class="pnsep">|</span>{}'.format(entry.payee, description)
-            change_str = balance_html(change)
-
-        elif isinstance(entry, Check):
-            # Check the balance here and possibly change the rowtype
-            if entry.errdiff is None:
-                description = 'Check {} has {}'.format(account_link(entry.account), entry.amount)
-            else:
-                description = 'Check in {} fails; expected = {}, balance = {}, difference = {}'.format(
-                    account_link(entry.account), entry.amount,
-                    balance.get_amount(entry.amount.currency),
-                    entry.errdiff)
-                rowtype = 'CheckFail'
-
-        elif isinstance(entry, (Open, Close)):
-            description = '{} {}'.format(entry.__class__.__name__, account_link(entry.account))
-
-        elif isinstance(entry, Note):
-            description = '{} {}'.format(entry.__class__.__name__, entry.comment)
-            change_str = ''
-            balance_str = ''
-
-        elif isinstance(entry, Document):
-            assert path.isabs(entry.filename)
-            description = 'Document for {}: "<a href="{}" class="filename">{}</a>"'.format(
-                account_link(entry.account),
-                app.router.build('doc', filename=entry.filename.lstrip('/')),
-                path.basename(entry.filename))
-            change_str = ''
-            balance_str = ''
-
-        else:
-            description = entry.__class__.__name__
-
-        # Render a row.
-        write('''
-          <tr class="{} {}" title="{}">
-            <td class="datecell">{}</td>
-            <td class="flag">{}</td>
-            <td class="description" colspan="5">{}</td>
-          <tr>
-        '''.format(rowtype, extra_class,
-                   '{}:{}'.format(entry.fileloc.filename, entry.fileloc.lineno),
-                   date, flag, description))
-
-        if render_postings and isinstance(entry, Transaction):
-            for posting in entry.postings:
-
-                classes = ['Posting']
-                if posting.flag == flags.FLAG_WARNING:
-                    classes.append('warning')
-
-                write('''
-                  <tr class="{}">
-                    <td class="datecell"></td>
-                    <td class="flag">{}</td>
-                    <td class="description">{}</td>
-                    <td class="amount num">{}</td>
-                    <td class="cost num">{}</td>
-                    <td class="price num">{}</td>
-                    <td class="balance num">{}</td>
-                  <tr>
-                '''.format(' '.join(classes),
-                           posting.flag or '',
-                           account_link(posting.account),
-                           posting.position.get_amount(),
-                           posting.position.lot.cost or '',
-                           posting.price or '',
-                           get_balance_amount(posting)))
-
-    write('</table>')
-
-
 @viewapp.route('/journal', name='journal')
-def journal():
+def journal_():
     "A list of all the entries in this realization."
     bottle.redirect(request.app.get_url('account', slashed_account_name=''))
 
@@ -833,7 +571,7 @@ def account(slashed_account_name=None):
     account_postings = realization.get_subpostings(real_accounts[account_name])
 
     oss = io.StringIO()
-    entries_table_with_balance(oss, account_postings)
+    journal.entries_table_with_balance(app, oss, account_postings)
     return render_app(
         pagetitle = '{}'.format(account_name), # Account:
         contents = oss.getvalue())
@@ -854,7 +592,7 @@ def conversions():
 
     oss = io.StringIO()
     conversion_entries = get_conversion_entries(view.entries)
-    entries_table(oss, conversion_entries, render_postings=True)
+    journal.entries_table(app, oss, conversion_entries, render_postings=True)
 
     balance = summarize.compute_total_balance(conversion_entries)
 
@@ -968,7 +706,7 @@ def documents():
     "Render a tree with all the documents found."
     document_entries = utils.filter_type(request.view.entries, Document)
     oss = io.StringIO()
-    entries_table(oss, document_entries)
+    journal.entries_table(app, oss, document_entries)
     return render_app(
         pagetitle = "Documents",
         contents = oss.getvalue())
