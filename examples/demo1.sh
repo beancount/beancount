01a147ac8bd746476763e83d82f2ccde6ba013f6
Martin Blais <blais@furius.ca>
2013-08-07 12:04:50 -0400

    Fixes due to watching another user try to install and run.

--- examples/demo1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# This command runs the beancount web server on a demo ledger file.
-# Run this in a shell and use a web browser to access http://localhost:8080
-# The demo file contains many example transactions.
-PROJDIR=.. . ../etc/env
-python $PROJDIR/bin/bean-web --with-source demo.ledger
--- src/python/beancount/__init__.py
+++ src/python/beancount/__init__.py
@@ -3,4 +3,10 @@
 This is v2, a complete rewrite of Beancount v1, simplified and improved
 drastically.
 """
+
+# Check the version requirements.
+import sys
+if (sys.version_info.major, sys.version_info.minor) < (3, 3):
+    raise ImportError("Python 3.3 or above is required.")
+
 from beancount.loader import load; 'public'
