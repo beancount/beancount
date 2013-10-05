56c0f11faa0e8d173f02717e58f803e27ce29ce8
Martin Blais <blais@furius.ca>
2013-10-05 10:12:45 -0400

    Added importer for RBC credit card PDF statements.

---
+++ src/python/beancount/sources/rbcpdf.py
@@ -0,0 +1,30 @@
+"""RBC Checking, Savings and Credit Card PDF statements importer.
+"""
+import re
+import datetime
+
+CONFIG = {
+    'FILE'       : 'Account for filing',
+}
+
+
+def import_date(filename, match_text):
+    """Try to get the date of the report from the filename."""
+    
+    if re.search("FileType: application/pdf", match_text):
+        if re.search("Contents:.*RBC ROYAL BANK.*CREDIT CARD PAYMENT CENTRE", match_text,
+                     re.DOTALL):
+            print(filename)
+
+            mo = re.match(r'(.*)/(\d+)-(\d\d\d\d)([A-Za-z]+)(\d\d)-(\d\d\d\d)([A-Za-z]+)(\d\d).pdf',
+                          filename)
+            if mo:
+                year, month, day = mo.group(6,7,8)
+                return datetime.date(int(year), MONTHS[month], int(day))
+
+        # FIXME: TODO - checking and savings accounts PDFs
+
+
+MONTHS = {month_str: (index+1)
+          for index, month_str in enumerate('Jan Feb Mar Apr May Jun '
+                                            'Jul Aug Sep Oct Nov Dec'.split())}
