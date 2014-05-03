9a256c279861b451dc80d115a8e98ebca0b462a7
Martin Blais <blais@furius.ca>
2014-05-03 18:08:13 -0400

    Removed dependency on beancount.core.flags.

--- src/python/beancount/core/flags.py
+++ src/python/beancount/core/flags.py
@@ -9,6 +9,3 @@ FLAG_SUMMARIZE   = 'S' # Transactions created due to summarization.
 FLAG_TRANSFER    = 'T' # Transactions created due to balance transfers.
 FLAG_CONVERSIONS = 'C' # Transactions created to account for price conversions.
 FLAG_UNREALIZED  = 'U' # Transactions created due to unrealized gains.
-
-# Default flag for imported transactions.
-FLAG_IMPORT = FLAG_OKAY
