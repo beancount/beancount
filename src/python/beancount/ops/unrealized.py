c302c55474e062c9a82d0611e8ab38fd52fb4f82
Martin Blais <blais@furius.ca>
2014-05-18 18:26:41 -0400

    Began testing work on beancount.ops.unrealized.

--- src/python/beancount/ops/unrealized.py
+++ src/python/beancount/ops/unrealized.py
@@ -2,8 +2,10 @@
 """
 import collections
 
-from beancount.core.amount import Decimal
+from beancount.core.amount import Decimal, ZERO
+from beancount.core import data
 from beancount.core import account
+from beancount.core.account_types import is_valid_account_name
 from beancount.core import amount
 from beancount.core.data import Transaction, Posting, FileLocation
 from beancount.core.position import Lot, Position
@@ -12,15 +14,37 @@ from beancount.ops import holdings
 from beancount.ops import prices
 
 
-def unrealized_gains(entries, subaccount_name, account_types):
-    """A function that inserts entries that represent unrealized gains, at the end
-    of the available history. Returns a new list of entries, with the new gains
-    inserted."""
+def add_unrealized_gains(entries, account_types, subaccount_name=None):
+    """Insert entries for unrealized capital gains.
+
+    This function inserts entries that represent unrealized gains, at the end of
+    the available history. It returns a new list of entries, with the new gains
+    inserted.
+
+    Args:
+      entries: A list of data directives.
+      account_types: An instance of account_types.AccountTypes, derived from
+        the options.
+      subaccount_name: An optional string, the name of a subaccount to create
+        under an account to book the unrealized gain. If this is left to its
+        default value, the gain is booked directly in the same account.
+    Returns:
+      A list of entries, which includes the new unrealized capital gains
+      entries at the end. The new list is still sorted.
+    Raises:
+      ValueError: If the subaccount name is not a valid account name component.
+    """
+    # Assert tha subaccount name is in valid format.
+    if subaccount_name:
+        validation_account = account.join(account_types.assets, subaccount_name)
+        if not is_valid_account_name(validation_account):
+            raise ValueError("Invalid subaccount name: '{}'".format(subaccount_name))
 
     if not entries:
         return entries
 
     # Group positions by (account, cost, cost_currency).
+    # FIXME: Make this use groupby.
     account_holdings = collections.defaultdict(list)
     for holding in holdings.get_final_holdings(entries):
         if not holding.cost_currency:
@@ -44,22 +68,35 @@ def unrealized_gains(entries, subaccount_name, account_types):
                                                     (currency, cost_currency),
                                                     latest_date)
 
+        # Note: since we're only considering positions held at cost, the
+        # transaction that created the position *must* have created at least one
+        # price point for that commodity, so we never expect for a price not to
+        # be available, which is reasoable.
+        assert price_number is not None, (currency, cost_currency, latest_date)
+
         # Compute the total number of units and book value for set of positions.
         total_units = Decimal()
         market_value = Decimal()
         book_value = Decimal()
         for holding in holdings_list:
-            number = holding.number
-            total_units += number
-            market_value += number * price_number
-            book_value += number * holding.cost_number
+            units = holding.number
+            total_units += units
+            market_value += units * price_number
+            book_value += units * holding.cost_number
 
+        # Compute the PnL; if there is no profit or loss, we create a
+        # corresponding entry anyway.
         pnl = market_value - book_value
+        average_cost = book_value / total_units
 
         # Create a new transaction to account for this difference in gain.
         fileloc = FileLocation('<unrealized_gains>', 0)
-        narration = "Unrealized gains for {} in {} (price: {}, as of {})".format(
-            currency, cost_currency, price_number, price_date)
+        narration = ("Unrealized {} for {} units of {} "
+                     "(price: {:.4f} {} as of {}, average cost: {:.4f} {})").format(
+                         "gain" if pnl > ZERO else "loss",
+                         total_units, currency,
+                         price_number, cost_currency, price_date,
+                         average_cost, cost_currency)
         entry = Transaction(fileloc, latest_date, flags.FLAG_UNREALIZED,
                             None, narration, None, None, [])
 
@@ -70,7 +107,6 @@ def unrealized_gains(entries, subaccount_name, account_types):
             asset_account = account_name
 
         # Note: don't set a price because we don't want these to end up in Conversions.
-        #price = amount.Amount(price_number, cost_currency)
         entry.postings.append(
             Posting(entry, asset_account,
                     Position(Lot(cost_currency, None, None), pnl),
@@ -81,6 +117,12 @@ def unrealized_gains(entries, subaccount_name, account_types):
         # Note: this is a rather convenient but arbitraty choice--maybe it would be best to
         # let the user decide to what account to book it, but I don't a nice way to let the
         # user specify this.
+        #
+
+        # FIXME: don't replace the assets account, replace any root account,
+        # issue warning if not assets or liabilities
+
+        # FIXME: merge this with the above
         income_account_name = account_name.replace(account_types.assets,
                                                    account_types.income)
         if subaccount_name:
@@ -91,9 +133,23 @@ def unrealized_gains(entries, subaccount_name, account_types):
         entry.postings.append(
             Posting(entry, income_account,
                     Position(Lot(cost_currency, None, None), -pnl),
-                    amount.Amount(price_number, cost_currency),
+                    None,
                     None))
 
         new_entries.append(entry)
 
     return entries + new_entries
+
+
+def get_unrealized_entries(entries):
+    """Return entries automatically created for unrealized gains.
+
+    Args:
+      entries: A list of directives.
+    Returns:
+      A list of directives, all of which are in the original list.
+    """
+    return [entry
+            for entry in entries
+            if (isinstance(entry, data.Transaction) and
+                entry.flag == flags.FLAG_UNREALIZED)]
---
+++ src/python/beancount/ops/unrealized_test.py
@@ -0,0 +1,189 @@
+import datetime
+import unittest
+import re
+import textwrap
+
+from beancount.core.amount import to_decimal, ZERO
+from beancount.core import account_types
+from beancount.core import data
+from beancount.parser import parsedoc, parse_string
+from beancount.parser import printer
+from beancount.parser import options
+from beancount.ops import unrealized
+
+
+def get_entries_with_narration(entries, regexp):
+    """Return the entries whose narration matches the regexp.
+
+    Args:
+      entries: A list of directives.
+      regexp: A regular expression string, to be matched against the
+        narration field of transactions.
+    Returns:
+      A list of directives.
+    """
+    return [entry
+            for entry in entries
+            if (isinstance(entry, data.Transaction) and
+                re.search(regexp, entry.narration))]
+
+
+class TestUnrealized(unittest.TestCase):
+
+    def test_empty_entries(self):
+        entries = unrealized.add_unrealized_gains(
+            [], account_types.DEFAULT_ACCOUNT_TYPES)
+        self.assertEqual([], entries)
+
+    @parsedoc
+    def test_nothing_held_at_cost(self, entries, _, options_map):
+        """
+        2014-01-01 open Assets:Account1
+        2014-01-01 open Assets:Account2
+        2014-01-01 open Income:Misc
+
+        2014-01-15 *
+          Income:Misc           -1000 USD
+          Assets:Account1
+
+        2014-01-16 *
+          Income:Misc           -1000 EUR
+          Assets:Account2
+
+        2014-02-01 price EUR  1.34 USD
+        """
+        new_entries = unrealized.add_unrealized_gains(
+            entries, options.get_account_types(options_map))
+        self.assertEqual(new_entries, entries)
+        self.assertEqual([],
+                         unrealized.get_unrealized_entries(new_entries))
+
+    @parsedoc
+    def test_normal_case(self, entries, _, options_map):
+        """
+        2014-01-01 open Assets:Account1
+        2014-01-01 open Assets:Account2
+        2014-01-01 open Income:Misc
+
+        2014-01-15 *
+          Income:Misc           -1000 USD
+          Assets:Account1       10 HOUSE {100 USD}
+
+        2014-01-16 *
+          Income:Misc           -600 USD
+          Assets:Account1       5 HOUSE {120 USD}
+
+        2014-01-17 *
+          Income:Misc           -1000 EUR
+          Assets:Account2       5 MANSION {200 EUR}
+
+        2014-01-18 * "Bought through a price conversion, not held at cost."
+          Income:Misc           -1500 EUR
+          Assets:Account2       5 HOTEL @ 300 EUR
+
+        2014-02-01 price HOUSE    130 USD
+        2014-02-01 price MANSION  180 EUR
+        2014-02-01 price HOTEL    330 USD
+        """
+        new_entries = unrealized.add_unrealized_gains(
+            entries, options.get_account_types(options_map))
+
+        self.assertEqual(2, len(unrealized.get_unrealized_entries(new_entries)))
+
+        house = get_entries_with_narration(new_entries, "units of HOUSE")[0]
+        self.assertEqual(2, len(house.postings))
+        self.assertEqual(to_decimal('350'), house.postings[0].position.number)
+        self.assertEqual('Assets:Account1', house.postings[0].account)
+        self.assertEqual('Income:Account1', house.postings[1].account)
+
+        mansion = get_entries_with_narration(new_entries, "units of MANSION")[0]
+        self.assertEqual(2, len(mansion.postings))
+        self.assertEqual(to_decimal('-100'), mansion.postings[0].position.number)
+
+    @parsedoc
+    def test_no_price(self, entries, _, options_map):
+        """
+        2014-01-01 open Assets:Account1
+        2014-01-01 open Income:Misc
+
+        2014-01-15 *
+          Income:Misc           -1000 USD
+          Assets:Account1       10 HOUSE {100 USD}
+        """
+        # Well... if there is a cost, there is at least one price, derived from
+        # the cost entry. This should always work.
+        new_entries = unrealized.add_unrealized_gains(
+            entries, options.get_account_types(options_map))
+        unreal_entries = unrealized.get_unrealized_entries(new_entries)
+        self.assertEqual(1, len(unreal_entries))
+        self.assertEqual(ZERO, unreal_entries[0].postings[0].position.number)
+
+    @parsedoc
+    def test_immediate_profit(self, entries, _, options_map):
+        """
+        2014-01-01 open Assets:Account1
+        2014-01-01 open Income:Misc
+
+        2014-01-15 *
+          Income:Misc           -1000 USD
+          Assets:Account1       10 HOUSE {100 USD} @ 120 USD
+        """
+        # Well... if there is a cost, there is at least one price, derived from
+        # the cost entry.
+        new_entries = unrealized.add_unrealized_gains(
+            entries, options.get_account_types(options_map))
+        unreal_entries = unrealized.get_unrealized_entries(new_entries)
+        self.assertEqual(1, len(unreal_entries))
+        self.assertEqual(to_decimal('200'),
+                         unreal_entries[0].postings[0].position.number)
+
+
+    @parsedoc
+    def test_conversions_only(self, entries, _, options_map):
+        """
+        2014-01-01 open Assets:Account1
+        2014-01-01 open Income:Misc
+
+        2014-01-15 *
+          Income:Misc           -780
+          Assets:Account1       600 EUR @ 1.3 USD
+        """
+        # Check to make sure values not held at cost are not included.
+        new_entries = unrealized.add_unrealized_gains(
+            entries, options.get_account_types(options_map))
+        self.assertEqual([], unrealized.get_unrealized_entries(new_entries))
+
+
+    @parsedoc
+    def test_with_subaccount(self, entries, _, options_map):
+        """
+        2014-01-01 open Assets:Account1
+        2014-01-01 open Income:Misc
+
+        2014-01-15 *
+          Income:Misc           -780
+          Assets:Account1       600 EUR @ 1.3 USD
+        """
+        with self.assertRaises(ValueError):
+            unrealized.add_unrealized_gains(
+                entries, options.get_account_types(options_map), '_invalid_')
+
+        # new_entries = unrealized.add_unrealized_gains(
+        #     entries, options.get_account_types(options_map), 'Gains')
+        # self.assertEqual([], unrealized.get_unrealized_entries(new_entries))
+
+
+
+
+
+# test with and without a subaccount
+
+# test with values held not in an assets account
+# (make the code replace the root account with an income account)
+
+
+# test the creation of a new income account
+
+
+
+# create a decorator for each of these methods that automatmically calls the function
