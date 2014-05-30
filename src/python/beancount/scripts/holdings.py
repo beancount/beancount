2b629b2fb7c0b9fd4920c1d4068391f3199a9586
Martin Blais <blais@furius.ca>
2014-05-30 01:36:42 -0400

    Implemented fractional/relative holdings reporting in order to share portfolio composition with buddies.

--- src/python/beancount/scripts/holdings.py
+++ src/python/beancount/scripts/holdings.py
@@ -21,6 +21,12 @@ def main():
                            action='store_true',
                            help="Aggregate by currency.")
 
+    # This is useful to share with other people the composition of your
+    # portfolio without sharing the absolute amounts.
+    optparser.add_argument('-r', '--relative', '--public',
+                           action='store_true',
+                           help="Only render relative amounts, not absolute.")
+
     optparser.add_argument('-f', '--format', default='txt',
                            choices=['txt', 'csv', 'html'],
                            help="Output format.")
@@ -38,22 +44,45 @@ def main():
                                                  account_types.liabilities),
                                                 price_map)
 
+    # Aggregate the holdings by (base, quote) pairs if requested.
     if opts.aggregated:
         holdings_list = holdings.aggregate_by_base_quote(holdings_list)
 
     if not holdings_list:
         return
 
-    # Create the table report.
-    field_spec = [('number', None, '{:,.2f}'.format),
-                  'currency',
-                  'cost_currency',
-                  ('cost_number', 'Average Cost', '{:,.2f}'.format),
-                  ('price_number', 'Average Price', '{:,.2f}'.format),
-                  ('book_value', None, '{:,.2f}'.format),
-                  ('market_value', None, '{:,.2f}'.format)]
+    # Decide what fields we will print out.
+    specs = {
+        'account'       : ('account', ),
+        'number'        : ('number', "Units", '{:,.2f}'.format),
+        'currency'      : ('currency', ),
+        'cost_currency' : ('cost_currency', ),
+        'cost_number'   : ('cost_number', 'Average Cost', '{:,.2f}'.format),
+        'price_number'  : ('price_number', 'Price', '{:,.2f}'.format),
+        'book_value'    : ('book_value', 'Book Value', '{:,.2f}'.format),
+        'market_value'  : ('market_value', 'Market Value', '{:,.2f}'.format),
+        'market_value%' : ('market_value', 'Market Value', '{:,.1%}'.format),
+    }
+
+    if opts.relative:
+        # Reduce the holdings to relative (fractional) values.
+        holdings_list = holdings.reduce_relative(holdings_list)
+
+        # Note: we do not include the book value in the output because an astute
+        # person could combine it with the market value percentage and use that
+        # to back out the total value of your portfolio.
+        field_spec = [specs[field]
+                      for field in ('currency cost_currency cost_number price_number '
+                                    'market_value%').split()]
+    else:
+        field_spec = [specs[field]
+                      for field in ('number currency cost_currency cost_number price_number '
+                                    'book_value market_value').split()]
+
     if not opts.aggregated:
         field_spec.insert(0, 'account')
+
+    # Create the table report.
     table_ = table.create_table(holdings_list, field_spec)
 
     # Render the table.
