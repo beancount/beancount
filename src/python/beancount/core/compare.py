d21c81cc5206a279e8452206ced372fbc4e8f312
Martin Blais <blais@furius.ca>
2014-06-01 11:15:59 -0400

    Implemented comparison operators.

---
+++ src/python/beancount/core/compare.py
@@ -0,0 +1,112 @@
+"""Comparison helpers for data objects.
+"""
+import hashlib
+import datetime
+from collections import namedtuple
+
+from .data import Transaction
+from .amount import Amount, Decimal, to_decimal
+from .position import Position, create_position
+from .account import has_component
+
+
+
+def stable_hash_namedtuple(objtuple, ignore=frozenset()):
+    """Hash the given namedtuple and its child fields.
+
+    The hash_obj is updated. This iterates over all the members of objtuple,
+    skipping the attributes from 'ignore', and if the elements are
+    lists or sets, sorts them for stability.
+
+    Args:
+      objtuple: A tuple object or other.
+      ignore: A set of strings, attribute names to be skipped in
+        computing a stable hash. For instance, circular references to objects
+        or irrelevant data.
+    """
+    hashobj = hashlib.md5()
+    for attr_name, attr_value in zip(objtuple._fields, objtuple):
+        if attr_name in ignore:
+            continue
+        if isinstance(attr_value, (list, set)):
+            subhashes = set()
+            for element in attr_value:
+                if isinstance(element, tuple):
+                    subhashes.add(stable_hash_namedtuple(element, ignore))
+                else:
+                    md5 = hashlib.md5()
+                    md5.update(str(element).encode())
+                    subhashes.add(md5.hexdigest())
+            for subhash in sorted(subhashes):
+                hashobj.update(subhash.encode())
+        else:
+            hashobj.update(str(attr_value).encode())
+    return hashobj.hexdigest()
+
+
+def hash_entries(entries):
+    """Compute unique hashes of each of the entries and return a map of them.
+
+    This is used for comparisons between sets of entries.
+
+    Args:
+      entries: A list of directives.
+    Returns:
+      A dict of hash-value to entry, for all entries.
+    """
+    entry_hash_dict = {}
+    for entry in entries:
+        entry_type = type(entry)
+
+        hash_ = stable_hash_namedtuple(entry, {'fileloc', 'entry'})
+        assert hash_ not in entry_hash_dict
+        entry_hash_dict[hash_] = entry
+
+    assert len(entry_hash_dict) == len(entries)
+    return entry_hash_dict
+
+
+def compare_entries(entries1, entries2):
+    """Compare two lists of entries. This is used for testing.
+
+    The entries are compared with disregard for their file location.
+
+    Args:
+      entries1: A list of directives of any type.
+      entries2: Another list of directives of any type.
+    Returns:
+      A tuple of (success, not_found1, not_found2), where the fields are:
+        success: A booelan, true if all the values are equal.
+        missing1: A list of directives from 'entries1' not found in
+          'entries2'.
+        missing2: A list of directives from 'entries2' not found in
+          'entries1'.
+    """
+    hashes1 = hash_entries(entries1)
+    hashes2 = hash_entries(entries2)
+    keys1 = set(hashes1.keys())
+    keys2 = set(hashes2.keys())
+
+    same = keys1 == keys2
+    missing1 = [hashes1[key] for key in keys1 - keys2]
+    missing2 = [hashes2[key] for key in keys2 - keys1]
+    return (same, missing1, missing2)
+
+
+def includes_entries(subset_entries, entries):
+    """Check if a list of entries is included in another list.
+
+    Args:
+      subset_entries: The set of entries to look for in 'entries'.
+      entries: The larger list of entries that could include 'subset_entries'.
+    Returns:
+      A boolean and a list of missing entries.
+    """
+    subset_hashes = hash_entries(subset_entries)
+    subset_keys = set(subset_hashes.keys())
+    hashes = hash_entries(entries)
+    keys = set(hashes.keys())
+
+    includes = subset_keys.issubset(keys)
+    missing = [subset_hashes[key] for key in subset_keys - keys]
+    return (includes, missing)
--- src/python/beancount/core/compare_test.py
+++ src/python/beancount/core/compare_test.py
@@ -3,6 +3,7 @@ import datetime
 import pprint
 
 from beancount.core import data
+from beancount.core import compare
 from beancount.parser import parser
 
 
@@ -35,6 +36,59 @@ TEST_INPUT = """
 class TestCompare(unittest.TestCase):
 
     def test_hash_entries(self):
-        entries, errors, options_map = parser.parse_string(TEST_INPUT)
-        entries_hash = data.hash_entries(entries)
-        pprint.pprint(sorted(entries_hash.keys()))
+        previous_hashes = None
+        for _ in range(64):
+            entries, errors, options_map = parser.parse_string(TEST_INPUT)
+            hashes = compare.hash_entries(entries)
+            if previous_hashes is None:
+                previous_hashes = hashes
+            else:
+                self.assertEqual(previous_hashes.keys(), hashes.keys())
+
+    def test_compare_entries(self):
+        entries1, _, __ = parser.parse_string(TEST_INPUT)
+        entries2, _, __ = parser.parse_string(TEST_INPUT)
+
+        # Check two equal sets.
+        same, missing1, missing2 = compare.compare_entries(entries1, entries2)
+        self.assertTrue(same)
+        self.assertFalse(missing1)
+        self.assertFalse(missing2)
+
+        # First > Second.
+        same, missing1, missing2 = compare.compare_entries(entries1, entries2[:-1])
+        self.assertFalse(same)
+        self.assertTrue(missing1)
+        self.assertFalse(missing2)
+        self.assertEqual(1, len(missing1))
+        self.assertTrue(isinstance(missing1.pop(), data.Close))
+
+        # First < Second.
+        same, missing1, missing2 = compare.compare_entries(entries1[:-1], entries2)
+        self.assertFalse(same)
+        self.assertFalse(missing1)
+        self.assertTrue(missing2)
+        self.assertEqual(1, len(missing2))
+        self.assertTrue(isinstance(missing2.pop(), data.Close))
+
+        # Both have missing.
+        same, missing1, missing2 = compare.compare_entries(entries1[1:], entries2[:-1])
+        self.assertFalse(same)
+        self.assertTrue(missing1)
+        self.assertTrue(missing2)
+        self.assertEqual(1, len(missing1))
+        self.assertTrue(isinstance(missing1.pop(), data.Close))
+        self.assertEqual(1, len(missing2))
+        self.assertTrue(isinstance(missing2.pop(), data.Open))
+
+    def test_includes_entries(self):
+        entries1, _, __ = parser.parse_string(TEST_INPUT)
+        entries2, _, __ = parser.parse_string(TEST_INPUT)
+
+        includes, missing  = compare.includes_entries(entries1[0:-3], entries2)
+        self.assertTrue(includes)
+        self.assertFalse(missing)
+
+        includes, missing  = compare.includes_entries(entries1, entries2[0:-3])
+        self.assertFalse(includes)
+        self.assertEqual(3, len(missing))
