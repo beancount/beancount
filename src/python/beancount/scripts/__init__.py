742aefa4d9c08cb3e3c2bdcf8a8bf485aab71ca9
Martin Blais <blais@furius.ca>
2014-06-01 11:29:54 -0400

    Added methods to base test case to compare/check for entries by streing.

--- src/python/beancount/scripts/__init__.py
+++ src/python/beancount/scripts/__init__.py
@@ -104,3 +104,65 @@ class TestCase(unittest.TestCase):
         with capture() as oss:
             yield oss
         self.assertLines(textwrap.dedent(expected_text), oss.getvalue())
+
+    def assertEqualEntries(self, expected_entries, actual_entries):
+        """Compare two lists of entries exactly and print missing entries verbosely if
+        they occur.
+
+        Args:
+          expected_entries: Either a list of directives or a string, in which case the
+            string is run through beancount.parser.parse_string() and the resulting
+            list is used.
+          actual_entries: Same treatment as expected_entries, the other list of directives to
+            compare to.
+        Raises:
+          AssertionError: If the exception fails.
+        """
+        if isinstance(expected_entries, str):
+            expected_entries, _, __ = parser.parse_string(expected_entries)
+        if isinstance(actual_entries, str):
+            actual_entries, _, __ = parser.parse_string(actual_entries)
+        same, expected_missing, actual_missing = copmare.compare_entries(expected_entries,
+                                                                         actual_entries)
+        if not same:
+            assert expected_missing or actual_missing
+            oss = io.StringIO()
+            if expected_missing:
+                oss.write("Missing from from first/expected set:\n\n")
+                for entry in expected_missing:
+                    oss.write(printer.format_entry(entry))
+                    oss.write('\n')
+            if actual_missing:
+                oss.write("Missing from from actual:\n\n")
+                for entry in actual_missing:
+                    oss.write(printer.format_entry(entry))
+                    oss.write('\n')
+            self.fail(oss.getvalue())
+
+    def assertIncludesEntries(self, expected_entries, actual_entries):
+        """Compare two lists of entries exactly and print missing entries verbosely if
+        they occur.
+
+        Args:
+          expected_entries: Either a list of directives or a string, in which case the
+            string is run through beancount.parser.parse_string() and the resulting
+            list is used.
+          actual_entries: Same treatment as expected_entries, the other list of directives to
+            compare to.
+        Raises:
+          AssertionError: If the exception fails.
+        """
+        if isinstance(expected_entries, str):
+            expected_entries, _, __ = parser.parse_string(expected_entries)
+        if isinstance(actual_entries, str):
+            actual_entries, _, __ = parser.parse_string(actual_entries)
+        includes, missing = copmare.includes_entries(expected_entries, actual_entries)
+        if not includes:
+            assert missing
+            oss = io.StringIO()
+            if missing:
+                oss.write("Missing from from first/expected set:\n\n")
+                for entry in missing:
+                    oss.write(printer.format_entry(entry))
+                    oss.write('\n')
+            self.fail(oss.getvalue())
