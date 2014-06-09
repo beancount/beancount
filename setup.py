0d586f6a6b9b77fb34750c9241c64f88c4281a0e
Martin Blais <blais@furius.ca>
2014-06-09 00:17:14 -0400

    Added missing javascript code to package_data from setup.py as well.

--- setup.py
+++ setup.py
@@ -32,44 +32,52 @@ bean-web
 
 
 setup(
-  name="beancount",
-  version='2.0beta',
-  description="Command-line Double-Entry Accounting",
-  long_description="""
-    A double-entry accounting system that uses a simple text file format
-    as input. A few Python scripts are used to parse the contents of the
-    file, for example, to serve the contents as a locally running web
-    server. Scripts are provided to convert from various input files into
-    Beancount's input format.
-  """,
-  license="GPL",
-  author="Martin Blais",
-  author_email="blais@furius.ca",
-  url="http://furius.ca/beancount",
+    name="beancount",
+    version='2.0beta',
+    description="Command-line Double-Entry Accounting",
 
-  # See note about about setuptools; uncomment if fixed.
-  ##install_requires = ['python-dateutil', 'BeautifulSoup4', 'lxml'],
+    long_description="""
+      A double-entry accounting system that uses a simple text file format
+      as input. A few Python scripts are used to parse the contents of the
+      file, for example, to serve the contents as a locally running web
+      server. Scripts are provided to convert from various input files into
+      Beancount's input format.
+    """,
 
-  package_dir = {'': 'src/python'},
-  packages = ['beancount',
-              'beancount.parser',
-              'beancount.core',
-              'beancount.ops',
-              'beancount.plugins',
-              'beancount.reports',
-              'beancount.scripts',
-              'beancount.web',
-              'beancount.utils',
-              ],
-  package_data = {'beancount.web': ['*.ico', '*.html', '*.css']},
-  scripts=install_scripts,
+    license="GPL",
+    author="Martin Blais",
+    author_email="blais@furius.ca",
+    url="http://furius.ca/beancount",
 
-  ext_modules=[
-      Extension("beancount/parser/_parser",
-                sources=[
-                    "src/python/beancount/parser/lexer.c",
-                    "src/python/beancount/parser/grammar.c",
-                    "src/python/beancount/parser/parser.c"
-                ]),
-  ],
+    # See note about about setuptools; uncomment if fixed.
+    ##install_requires = ['python-dateutil', 'BeautifulSoup4', 'lxml'],
+
+    package_dir = {'': 'src/python'},
+    packages = ['beancount',
+                'beancount.parser',
+                'beancount.core',
+                'beancount.ops',
+                'beancount.plugins',
+                'beancount.reports',
+                'beancount.scripts',
+                'beancount.web',
+                'beancount.utils'],
+
+    package_data = {
+        'beancount.web': ['*.ico',
+                          '*.html',
+                          '*.css',
+                          'third_party/*.js'],
+        },
+
+    scripts=install_scripts,
+
+    ext_modules=[
+        Extension("beancount/parser/_parser",
+                  sources=[
+                      "src/python/beancount/parser/lexer.c",
+                      "src/python/beancount/parser/grammar.c",
+                      "src/python/beancount/parser/parser.c"
+                  ]),
+    ],
 )
