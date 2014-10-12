7ed868d600f5483806c3fee801605b83fecc003b
Martin Blais <blais@furius.ca>
2014-10-12 19:27:59 -0400

    Completed generating files for tutorial.

--- src/python/beancount/scripts/tutorial.py
+++ src/python/beancount/scripts/tutorial.py
@@ -8,19 +8,30 @@ from os import path
 
 
 COMMANDS = [
-    ('balances'               , "bean-query {} balances"),
-    ('help-reports'           , "bean-query --help-reports"),
-    ('help-subcmd'            , "bean-query {} balances --help"),
-    ('help-global'            , "bean-query --help"),
-    ('help-formats'           , "bean-query --help-formats"),
-    ('balances-restrict'      , "bean-query {} balances -e ETrade"),
-    ('balances-restrict-cost' , "bean-query {} balances -e ETrade -c"),
-    ('balsheet'               , "bean-query {} balsheet"),
-
-    # ('journal'              , "bean-query {} journal -a Assets:US:BofA:Checking"),
-    # ('journal-with-balance' , "bean-query {} journal -a Assets:US:BofA:Checking -b"),
-    # ('invest'               , "bean-query {} journal -a Assets:US:Vanguard:VBMPX -b"),
-    # ('invest-with-cost'     , "bean-query {} journal -a Assets:US:Vanguard:VBMPX -b -c"),
+    ('balances'                 , "bean-query {} balances"),
+    ('help-reports'             , "bean-query --help-reports"),
+    ('help-subcmd'              , "bean-query {} balances --help"),
+    ('help-global'              , "bean-query --help"),
+    ('help-formats'             , "bean-query --help-formats"),
+    ('balances-restrict'        , "bean-query {} balances -e ETrade"),
+    ('balances-restrict-cost'   , "bean-query {} balances -e ETrade -c"),
+    ('balsheet'                 , "bean-query {} balsheet"),
+    ('journal'                  , "bean-query {} journal -w 120 -a Assets:US:BofA:Checking"),
+    ('journal-with-balance'     , "bean-query {} journal -w 120 -a Assets:US:BofA:Checking -b"),
+    ('invest'                   , "bean-query {} journal -w 120 -a Assets:US:ETrade:GLD -b"),
+    ('invest-with-cost'         , "bean-query {} journal -w 120 -a Assets:US:ETrade:GLD -b -c"),
+    ('journal-unrestricted'     , "bean-query {} journal -w 120 -b"),
+    ('holdings'                 , "bean-query {} holdings"),
+    ('holdings-by-account'      , "bean-query {} holdings --by account"),
+    ('holdings-by-root-account' , "bean-query {} holdings --by root-account"),
+    ('holdings-by-commodity'    , "bean-query {} holdings --by commodity"),
+    ('holdings-by-currency'     , "bean-query {} holdings --by currency"),
+    ('networth'                 , "bean-query {} networth"),
+    ('accounts'                 , "bean-query {} accounts"),
+    ('events'                   , "bean-query {} events"),
+    ('stats-directives'         , "bean-query {} stats-directives"),
+    ('stats-postings'           , "bean-query {} stats-postings"),
+    ('holdings-csv'             , "bean-query -f csv {} holdings"),
     ]
 
 
@@ -46,8 +57,6 @@ def main():
 
         if path.getsize(errors_filename) == 0:
             os.remove(errors_filename)
-
-
 
 
 if __name__ == '__main__':
