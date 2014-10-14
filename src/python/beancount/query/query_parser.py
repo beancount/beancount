8f00cc1508572d9ccca07d53535aff2cc2a7148c
Martin Blais <blais@furius.ca>
2014-10-14 01:20:07 -0400

    Basic working prototype for FROM clause, functions, lots more.

--- src/python/beancount/query/query_parser.py
+++ src/python/beancount/query/query_parser.py
@@ -6,35 +6,276 @@ import itertools
 import ply.lex
 import ply.yacc
 
+from beancount.core import position
+
+
+class Column:
+    def __init__(self, name):
+        self.name = name
+
+class TypeColumn(Column):
+    def __call__(self, posting):
+        return type(posting.entry)
+
+class FilenameColumn(Column):
+    def __call__(self, posting):
+        return posting.entry.source.filename
+
+class LineNoColumn(Column):
+    def __call__(self, posting):
+        return posting.entry.source.lineno
+
+class DateColumn(Column):
+    def __call__(self, posting):
+        return posting.entry.date
+
+class YearColumn(Column):
+    def __call__(self, posting):
+        return posting.entry.date.year
+
+class FlagColumn(Column):
+    def __call__(self, posting):
+        return posting.entry.flag
+
+class PayeeColumn(Column):
+    def __call__(self, posting):
+        return posting.entry.payee
+
+class NarrationColumn(Column):
+    def __call__(self, posting):
+        return posting.entry.narration
+
+class TagsColumn(Column):
+    def __call__(self, posting):
+        return posting.entry.tags
+
+class LinksColumn(Column):
+    def __call__(self, posting):
+        return posting.entry.links
+
+class AccountColumn(Column):
+    def __call__(self, posting):
+        return posting.account
+
+class NumberColumn(Column):
+    def __call__(self, posting):
+        return posting.position.number
+
+class CurrencyColumn(Column):
+    def __call__(self, posting):
+        return posting.position.lot.currency
+
+class ChangeColumn(Column):
+    def __call__(self, posting):
+        return posting.position
+
+
+POSTING_COLUMNS = {
+    'type'      : TypeColumn,
+    'filename'  : FilenameColumn,
+    'lineno'    : LineNoColumn,
+    'date'      : DateColumn,
+    'year'      : YearColumn,
+    'flag'      : FlagColumn,
+    'payee'     : PayeeColumn,
+    'narration' : NarrationColumn,
+    'tags'      : TagsColumn,
+    'links'     : LinksColumn,
+    'account'   : AccountColumn,
+    'number'    : NumberColumn,
+    'currency'  : CurrencyColumn,
+    'change'    : ChangeColumn,
+    }
+
+
+
+class Function(Column):
+    def __init__(self, child, **kwds):
+        super().__init__(**kwds)
+        self.child = child
+
+class Length(Function):
+    def __call__(self, posting):
+        child_value = self.child(posting)
+        return len(child_value)
+
+class Units(Function):
+    def __call__(self, posting):
+        position_ = self.child(posting)
+        assert isinstance(position_, position.Position)
+        return position_.get_amount()
+
+class Cost(Function):
+    def __call__(self, posting):
+        position_ = self.child(posting)
+        assert isinstance(position_, position.Position)
+        return position_.get_cost()
+
+
+class Aggregator(Function):
+    def __init__(self, **kwds):
+        super().__init__(**kwds)
+        self.agg_value = None
+
+class Sum(Aggregator):
+    def __call__(self, posting):
+        child_value = self.child(posting)
+        if self.agg_value is None:
+            self.agg_value = child_value
+        else:
+            self.agg_value += child_value
+
+class First(Aggregator):
+    def __call__(self, posting):
+        if self.agg_value is None:
+            self.agg_value = self.child(posting)
+
+class Last(Aggregator):
+    def __call__(self, posting):
+        self.agg_value = self.child(posting)
+
+
+FUNCTIONS = {
+    'length': Length,
+    'units': Units,
+    'cost': Cost,
+    'sum': Sum,
+    'first': First,
+    'last': Last,
+    }
+
+
+
+class DateEntryColumn(Column):
+    def __call__(self, entry):
+        return entry.date
+
+class FlagEntryColumn(Column):
+    def __call__(self, entry):
+        return entry.flag
+
+# FIXME: These ought to be functions, as in YEAR(date) or MONTH(date), that work
+# on both types of entries (they would work on dates, really).
+class YearEntryColumn(Column):
+    def __call__(self, entry):
+        return entry.date.year
+
+class MonthEntryColumn(Column):
+    def __call__(self, entry):
+        return entry.date.month
+
+ENTRY_COLUMNS = {
+    'date'  : DateEntryColumn,
+    'flag'  : FlagEntryColumn,
+    'year'  : YearEntryColumn,
+    'month' : MonthEntryColumn,
+    }
+
+
+
+Query = collections.namedtuple('Query',
+                               'entry_filter posting_filter targets')
+
+
+class Expr:
+    def __call__(self, context):
+        raise NotImplementedError
+
+class Tauto(Expr):
+    def __call__(self, _):
+        return True
+
+class Op(Expr):
+    def __init__(self, child):
+        self.child = child
+
+class Not(Op):
+    def __call__(self, context):
+        return not self.child(context)
+
+class Eq(Op):
+    def __init__(self, child, value):
+        super().__init__(child)
+        self.value = value
+
+    def __call__(self, context):
+        return self.child(context) == self.value
+
+class Ne(Op):
+    def __init__(self, child, value):
+        super().__init__(child)
+        self.value = value
+
+    def __call__(self, context):
+        return self.child(context) != self.value
+
+class BinOp(Expr):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class And(BinOp):
+    def __call__(self, context):
+        return (self.left(context) and
+                self.right(context))
+
+class Or(BinOp):
+    def __call__(self, context):
+        return (self.left(context) or
+                self.right(context))
+
+
+
 
 class ParseError(Exception):
     """A parser error."""
 
 
 class Lexer:
-    """Lexer for the Beancount Query Language.
+    """PLY lexer for the Beancount Query Language.
     """
 
     # List of reserved keywords.
     keywords = {
         'SELECT', 'FROM', 'WHERE',
         # 'GROUP', 'ORDER', 'BY', 'LIMIT', 'DESC', 'ASC',
-        # 'AND', 'OR', 'NOT'
+        'AND', 'OR', 'NOT',
+        'NULL',
     }
 
     # List of valid tokens from the lexer.
-    tokens = ['ID', 'COMMA'] + list(keywords)
+    tokens = [
+        'ID', 'INTEGER', 'STRING',
+        'COMMA', 'SEMI', 'LPAREN', 'RPAREN', 'EQ', 'NE'
+    ] + list(keywords)
 
     # An identifier, for a target or a dimension or whatever.
     def t_ID(self, token):
         "[a-zA-Z][a-zA-Z_]*"
         utoken = token.value.upper()
-        token.type = utoken if utoken in self.keywords else 'ID'
+        if utoken in self.keywords:
+            token.type = utoken
         token.value = token.value.lower()
         return token
 
+    def t_STRING(self, token):
+        "(\"[^\"]*\"|\'[^\']*\')"
+        token.value = token.value[1:-1]
+        return token
+
     # Constant tokens.
-    t_COMMA = ","
+    t_COMMA  = r","
+    t_SEMI   = r";"
+    t_LPAREN = r"\("
+    t_RPAREN = r"\)"
+    t_NE     = r"!="
+    t_EQ     = r"="
+
+    # Numbers.
+    def t_INTEGER(self, token):
+        r"[0-9]+"
+        token.value = int(token.value)
+        return token
 
     # Ignore whitespace.
     t_ignore = " \t\n"
@@ -44,42 +285,47 @@ class Lexer:
         raise ParseError("Unknown token: {}".format(token))
 
 
-Select = collections.namedtuple('Select',
-                                'columns from_clause where_clause')
-
-
-class Parser:
-
-    # FIXME: Maybe just derivation will work here, try it, try avoiding
-    # instantiation of the lexer.
-    tokens = Lexer.tokens
+class Parser(Lexer):
+    """PLY parser for the Beancount Query Language.
+    """
 
     # Starting rule.
     start = 'select_statement'
 
     def __init__(self, **options):
-        self.lexer = create_lexer()
+        self.ply_lexer = ply.lex.lex(module=self,
+                                     optimize=False)
         self.ply_parser = ply.yacc.yacc(module=self,
                                         optimize=False,
                                         write_tables=False,
                                         debug=False,
                                         **options)
 
+    def tokenize(self, line):
+        self.ply_lexer.input(line)
+        while 1:
+            tok = self.ply_lexer.token()
+            if not tok:
+                break
+            print(tok)
+
     def parse(self, line, debug=False):
-        return self.ply_parser.parse(line.strip().rstrip(';'),
-                                     lexer=self.lexer,
+        return self.ply_parser.parse(line,
+                                     lexer=self.ply_lexer,
                                      debug=debug)
 
     def p_select_statement(self, p):
         """
-        select_statement : SELECT id_list opt_from opt_where
+        select_statement : SELECT target_list opt_from opt_where SEMI
         """
-        p[0] = Select(p[2], p[3], p[4])
+        p[0] = Query(p[3] or Tauto(),
+                     p[4] or Tauto(),
+                     p[2])
 
-    def p_id_list(self, p):
+    def p_target_list(self, p):
         """
-        id_list : ID
-                | id_list COMMA ID
+        target_list : target
+                    | target_list COMMA target
         """
         if len(p) == 2:
             p[0] = [p[1]]
@@ -87,50 +333,142 @@ class Parser:
             p[0] = p[1]
             p[0].append(p[3])
 
+    def p_target(self, p):
+        """
+        target : funcall
+               | ID
+        """
+        if isinstance(p[1], str):
+            identifier = p[1]
+            try:
+                cls = POSTING_COLUMNS[identifier]
+                p[0] = cls(name=identifier)
+            except KeyError:
+                raise ParseError("Invalid column name: '{}'".format(identifer))
+        else:
+            p[0] = p[1]
+
+    def p_function(self, p):
+        """
+        funcall : ID LPAREN target RPAREN
+        """
+        function_name = p[1]
+        try:
+            FunctionType = FUNCTIONS[function_name]
+            p[0] = FunctionType(child=p[3], name=function_name)
+        except KeyError:
+            raise ParseError("Invalid function name: '{}'".format(function_name))
+
+    # FIXME: Differentiate between entry expresssions and posting expressions
     def p_opt_from(self, p):
         """
         opt_from : empty
-                 | FROM from_expr
+                 | FROM expression
         """
-        if len(p) > 2:
+        if len(p) == 3:
+            assert p[2], "Empty FROM clause is not allowed"
             p[0] = p[2]
 
     def p_opt_where(self, p):
         """
         opt_where : empty
-                  | WHERE where_expr
+                  | WHERE expression
         """
-        if len(p) > 2:
+        if len(p) == 3:
+            assert p[2], "Empty WHERE clause is not allowed"
             p[0] = p[2]
 
-    def p_from_expr(self, p):
-        """
-        from_expr : empty
-        """
+    precedence = [
+        ('left', 'EQ', 'NE'),
+        ('left', 'OR'),
+        ('left', 'AND'),
+        ('left', 'NOT'),
+        ]
 
-    def p_where_expr(self, p):
+    def p_expression(self, p):
         """
-        where_expr : empty
+        expression : expression AND expression
+                   | expression OR expression
+                   | NOT expression
+                   | LPAREN expression RPAREN
+                   | predicate
         """
+        if len(p) == 2:
+            # predicate
+            p[0] = p[1]
+        elif len(p) == 3:
+            # NOT expression
+            p[0] = Not(p[2])
+        elif p[1] == '(':
+            p[0] = p[2]
+        elif p[2].upper() == 'AND':
+            p[0] = And(p[1], p[3])
+        elif p[2].upper() == 'OR':
+            p[0] = Or(p[1], p[3])
+        else:
+            assert False, "Internal error"
+
+    def p_predicate(self, p):
+        """
+        predicate : entry_target EQ value
+                  | entry_target NE value
+        """
+        if p[2] == '=':
+            p[0] = Eq(p[1], p[3])
+        elif p[2] == '!=':
+            p[0] = Ne(p[1], p[3])
+        else:
+            assert False, "Internal error"
+
+    def p_value(self, p):
+        """
+        value : NULL
+              | INTEGER
+              | STRING
+        """
+        if p[1] == 'NULL':
+            p[0] = None
+        else:
+            p[0] = p[1]
+
+    def p_entry_target(self, p):
+        """
+        entry_target : ID
+        """
+        identifier = p[1]
+        try:
+            cls = ENTRY_COLUMNS[identifier]
+            p[0] = cls(name=identifier)
+        except KeyError:
+            raise ParseError("Invalid entry column name: '{}'".format(identifier))
 
     def p_empty(self, _):
         """
         empty :
         """
-
     def p_error(self, context):
         raise ParseError("Syntax error: {}".format(context))
 
 
-def create_lexer():
-    return ply.lex.lex(module=Lexer(), optimize=False)
 
-
-        #     # Give the lexer some input
-        # self.lexer.input(line)
-
-        # # Tokenize
-        # while True:
-        #     tok = self.lexer.token()
-        #     if not tok: break      # No more input
-        #     print(tok)
+# FIXME:
+# - begin implementing unit tests
+# - tokenize and parse all data types, including dates
+# - differentiate clearly between entry and posting columns
+# - differentiate clearly between entry and posting functions
+# - implement inequalities and regexp matching
+# - implement aggregation
+# - render with custom routine, not beancount.reports.table
+# - deal with rendering on multiple lines, e.g., for inventories with multiple positions
+# - make it possible to run from the command-line (batch)
+# - check data types for functions
+# - invoke a pager when long output
+# - implement set variables for format and verbosity and display precision and what-not
+# - implement JOURNAL account FROM
+# - implement BALANCES FROM
+# - find a way to pipe into treeify
+# - find a way to trigger a close in the FROM clause
+# - implement order by
+# - implement limit
+# - implement distinct
+# - support simple boolean expressions in filter expressions, not just equalities and inequalities
--- src/python/beancount/query/shell.py
+++ src/python/beancount/query/shell.py
@@ -1,13 +1,17 @@
 """An interactive command-line shell interpreter for the Beancount Query Language.
 """
+import collections
 import os
 import sys
 import cmd
 import readline
 import atexit
+import traceback
 from os import path
 
 from beancount.query import query_parser
+from beancount.core import data
+from beancount.reports import table
 
 
 HISTORY_FILENAME = "~/.bean-shell-history"
@@ -42,18 +46,43 @@ def save_history(filename):
 class BQLShell(cmd.Cmd):
     """An interactive shell interpreter for the Beancount query language.
     """
-    def __init__(self):
+    prompt = 'beancount> '
+
+    def __init__(self, entries):
         super().__init__()
         load_history(path.expanduser(HISTORY_FILENAME))
         self.parser = query_parser.Parser()
+        self.entries = entries
 
     def do_select(self, line_rest):
         line = 'SELECT ' + line_rest
         try:
             select = self.parser.parse(line)
-            print(select)
+            process_select(self.entries, select)
         except query_parser.ParseError as exc:
             print(exc)
+        except Exception as exc:
+            traceback.print_exc()
+
+    def do_lex(self, line):
+        """Debug lexer."""
+        try:
+            self.parser.tokenize(line)
+        except query_parser.ParseError as exc:
+            print(exc)
+    do_tokenize = do_lex
+
+    def do_parse(self, line):
+        """Debug parser."""
+        print("INPUT: {}".format(repr(line)))
+        try:
+            result = self.parser.parse(line, True)
+            print(result)
+        except query_parser.ParseError as exc:
+            print(exc)
+        except Exception as exc:
+            traceback.print_exc()
+    do_explain = do_parse
 
     def emptyline(self):
         # Do nothing on an empty line.
@@ -70,15 +99,61 @@ class BQLShell(cmd.Cmd):
         return 1
 
 
-def run_noargs():
+def run_noargs(entries):
     """Create and run a shell, possibly consuming stdin if not interactive.
     If we're running in a TTY, start an interactive shell.
+
+    Args:
+      entries: A list of directives.
     """
-    shell = BQLShell()
+    shell = BQLShell(entries)
     if os.isatty(sys.stdin.fileno()):
         # If we're a TTY, run interactively.
+        print("Ready with {} entries.".format(len(entries)))
         shell.cmdloop()
     else:
         # If we're not a TTY, read the BQL command from standard input.
         script = sys.stdin.read()
         pass ## FIXME: TODO - shell.process_command(script)
+
+
+def process_select(entries, query):
+    """Process a select query.
+
+    Args:
+      entries: A list of directives.
+      query: An instance of Query.
+    """
+
+    # Create a class for the row.
+    Tuple = collections.namedtuple('Tuple',
+                                   [target.name for target in query.targets])
+
+    # Filter the entries.
+    if query.entry_filter:
+        expression = query.entry_filter
+        filtered_entries = []
+        for entry in entries:
+            if isinstance(entry, data.Transaction):
+                if expression(entry):
+                    filtered_entries.append(entry)
+            else:
+                filtered_entries.append(entry)
+    else:
+        filtered_entries = entries
+
+    # Process all the postings.
+    rows = []
+    expression = query.posting_filter
+    for entry in filtered_entries:
+        if isinstance(entry, data.Transaction):
+            for posting in entry.postings:
+                if expression(posting):
+                    row = Tuple(*[target(posting) for target in query.targets])
+                    rows.append(row)
+
+    if not rows:
+        print("(empty)")
+    else:
+        table_ = table.create_table(rows)
+        table.render_table(table_, sys.stdout, 'text')
