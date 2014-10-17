c4f4b2799cb120099ec2a5d073cf6edda02d7d5f
Martin Blais <blais@furius.ca>
2014-10-17 09:47:23 -0400

    Clean up doc in order to share pristine example w/ TOliphant.

--- experiments/download-docs/download-docs.py
+++ experiments/download-docs/download-docs.py
@@ -23,6 +23,56 @@ import httplib2
 import googleapiclient.discovery
 
 
+def create_flow_from_file(scope):
+    """Create a flow from data stored in a filename.
+
+    Args:
+      scope: A string, the scope to get credentials for.
+    Returns:
+      A flow object.
+    """
+    secrets_filename = os.environ['GOOGLE_APIS']
+    flow = oauth2client.client.flow_from_clientsecrets(secrets_filename, scope)
+    flow.redirect_uri = oauth2client.client.OOB_CALLBACK_URN
+
+    # Secrets file contents look like this:
+    #
+    # {"installed":
+    #  {"client_id": ".....",
+    #   "client_secret": ".....",
+    #   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    #   "token_uri": "https://accounts.google.com/o/oauth2/token",
+    #   "client_email": "",
+    #   "redirect_uris": ["urn: ietf: wg: oauth: 2.0: oob", "oob"],
+    #   "client_x509_cert_url": "",
+    #   "auth_provider_x509_cert_url": "https: //www.googleapis.com/oauth2/v1/certs"}}
+
+    return flow
+
+
+# Hardcoded secrets baked here for my native app.
+CLIENT_ID     = '.....' # Removed.
+CLIENT_SECRET = '.....' # Removed.
+
+def create_flow_from_ids(scope):
+    """Create a flow from hardcoded IDs.
+
+    Args:
+      scope: A string, the scope to get credentials for.
+    Returns:
+      A flow object.
+    """
+    secrets_filename = os.environ['GOOGLE_APIS']
+    flow = oauth2client.client.flow_from_clientsecrets(secrets_filename, scope)
+    flow.redirect_uri = oauth2client.client.OOB_CALLBACK_URN
+
+    # Note: You could also have used 'http://localhost' to redirect.
+    return OAuth2WebServerFlow(client_id=CLIENT_ID,
+                               client_secret=CLIENT_SECRET,
+                               scope=scope,
+                               redirect_uri='urn:ietf:wg:oauth:2.0:oob')
+
+
 def get_authenticated_http(scope, storage_filename, opts):
     """Authenticate via oauth2 and store to a filename.
     If credentials are already available in the storage filename, this does not
@@ -37,34 +87,7 @@ def get_authenticated_http(scope, storage_filename, opts):
     Returns:
       An authenticated http client object, from which you can use the Google APIs.
     """
-
-    if 1:
-        # With secrets stored in a file.
-        secrets_filename = os.environ['GOOGLE_APIS']
-        flow = oauth2client.client.flow_from_clientsecrets(secrets_filename, scope)
-        flow.redirect_uri = oauth2client.client.OOB_CALLBACK_URN
-
-        # Secrets file contents look like this:
-        #
-        # {"installed":
-        #  {"client_id": ".....",
-        #   "client_secret": ".....",
-        #   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        #   "token_uri": "https://accounts.google.com/o/oauth2/token",
-        #   "client_email": "",
-        #   "redirect_uris": ["urn: ietf: wg: oauth: 2.0: oob", "oob"],
-        #   "client_x509_cert_url": "",
-        #   "auth_provider_x509_cert_url": "https: //www.googleapis.com/oauth2/v1/certs"}}
-    else:
-        # With secrets baked here for my native app.
-        CLIENT_ID = '.....'
-        CLIENT_SECRET = '.....'
-
-        # Note: You could also have used 'http://localhost' to redirect.
-        flow = OAuth2WebServerFlow(client_id=CLIENT_ID,
-                                   client_secret=CLIENT_SECRET,
-                                   scope=scope,
-                                   redirect_uri='urn:ietf:wg:oauth:2.0:oob')
+    flow = create_flow_from_ids(scope)
 
     http = httplib2.Http()
     http.disable_ssl_certificate_validation = True
@@ -138,52 +161,6 @@ def download_docs(drive, tempdir):
     return filenames
 
 
-
-def experiment(drive):
-    """Toy around and experiment with the Google Drive API.
-
-    Args:
-      drive: A googleapiclient Drive stub.
-    """
-
-    # about() API.
-    if 0:
-        about = drive.about().get().execute()
-        pp(about['name'])
-
-    # files() and children() API.
-    if 0:
-        filelist = drive.files().list(q="'0Bxq3M95KLnV5VHdUQnRVUVRJM1k' in parents", maxResults=1000).execute()
-        #pp(filelist)
-        # filelist = drive.children().list(folderId='0Bxq3M95KLnV5VHdUQnRVUVRJM1k', maxResults=1000).execute()
-        assert 'nextPageToken' not in filelist
-        for item in filelist['items']:
-            print "{:48} {:64} {}".format(*map(item.get, 'id mimeType title'.split()))
-            if 0:
-                print
-                pp(item.get('webContentLink'))
-                pp(item.get('alternateLink'))
-                pp(item.get('downloadUrl'))
-
-
-    # Download a file's contents in various formats.
-    index_id = '1RaondTJCS_IUPBHFNdT8oqFKJjVJDsfsn6JEjBG04eA'
-    returns_id = '1vEFB44-HFqydYVJXA-QxT4AN0K5xHHfSkhTmRSCPkac'
-    id_ = index_id
-
-    index_metadata = drive.files().get(fileId=id_).execute()
-    pp(index_metadata)
-    for mime_type in ['application/pdf',
-                      'text/html',
-                      'text/plain',
-                      'application/rtf',
-                      'application/vnd.oasis.opendocument.text']:
-        url = index_metadata['exportLinks'][mime_type]
-        filename = '/tmp/index.{}'.format(mime_type.replace('/', '.'))
-        filename, unused_headers = urllib.urlretrieve(url, filename)
-        print filename
-
-
 def main():
     logging.basicConfig(level=logging.INFO, format='%(levelname)-8s: %(message)s')
 
@@ -207,8 +184,6 @@ def main():
 
     # Access the drive API.
     drive = googleapiclient.discovery.build('drive', 'v2', http=http)
-
-    #experiment(drive)
 
     try:
         # Download the docs.
