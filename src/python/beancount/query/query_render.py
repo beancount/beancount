ed8621c08273577a02dbc7b4f7324e2bf8061ce3
Martin Blais <blais@furius.ca>
2014-11-03 18:12:00 -0500

    Created column renderers that align the data nicely.

--- src/python/beancount/query/query_render.py
+++ src/python/beancount/query/query_render.py
@@ -3,11 +3,14 @@
 import collections
 import datetime
 import itertools
+import math
 from itertools import zip_longest
 
 from beancount.core.amount import Decimal
+from beancount.core.amount import ZERO
 from beancount.core import data
 from beancount.core import inventory
+from beancount.core import position
 from beancount.query import query_compile
 from beancount.parser import options
 from beancount.parser import printer
@@ -15,6 +18,267 @@ from beancount.ops import summarize
 from beancount.utils import misc_utils
 from beancount.utils.misc_utils import box
 from beancount.reports import table
+
+
+class ColumnRenderer:
+    """Base class for classes that render and compute formatting and width for all
+    values that appear within a column. All the values rendered are assumed to
+    be of the same type, or None (empty). The class receives all the values that
+    will be rendered to accumulate the dimensions it will need to format them
+    later on. It is then responsible to render those values in a way that will
+    align nicely in a column, in the rendered output, whereby all the values
+    render to the same width.
+    """
+    # Override, the type of object to be rendered.
+    dtype = None
+
+    def update(self, value):
+        """Update the rendered with the given value.
+        Args:
+          value: Any object of the type 'dtype'.
+        Returns:
+          An integer, the number of lines this will get rendered to.
+        """
+        raise NotImplementedError
+
+    def prepare(self):
+        """Prepare to render all values of a column."""
+        # No-op. Override if desired.
+
+    def width(self):
+        """Return the computed width of this column.
+        Returns:
+          An integer, the number of characters wide required for this field.
+        """
+        raise NotImplementedError
+
+    def format(self, value):
+        """Format the value.
+
+        Args:
+          value: Any object of the type 'dtype'.
+        Returns:
+          A string, or a list of strings, the rendered and aligned string(s)
+          representations of for the value. A value may render on multiple
+          lines, which is why a list may be returned here.
+        """
+        raise NotImplementedError
+
+
+class StringRenderer(ColumnRenderer):
+    """A renderer for left-aligned strings."""
+    dtype = str
+
+    def __init__(self):
+        self.maxlen = 0
+
+    def update(self, string):
+        if string is not None:
+            self.maxlen = max(self.maxlen, len(string))
+
+    def prepare(self):
+        self.fmt = '{{:<{}.{}}}'.format(self.maxlen, self.maxlen)
+
+    def width(self):
+        return self.maxlen
+
+    def format(self, string):
+        return self.fmt.format('' if string is None else string)
+
+
+class DateTimeRenderer(ColumnRenderer):
+    """A renderer for decimal numbers."""
+    dtype = datetime.date
+
+    def __init__(self):
+        self.empty = ' ' * 10
+
+    def update(self, _):
+        pass
+
+    def width(self):
+        return 10
+
+    def format(self, dtime):
+        return self.empty if dtime is None else dtime.strftime('%Y-%m-%d')
+
+
+class DecimalRenderer(ColumnRenderer):
+    """A renderer for decimal numbers."""
+    dtype = Decimal
+
+    def __init__(self):
+        self.has_negative = False
+        self.max_adjusted = 0
+        self.min_exponent = 0
+        self.total_width = None
+
+    def update(self, number):
+        if number is None:
+            return
+        ntuple = number.as_tuple()
+        if ntuple.sign:
+            self.has_negative = True
+        self.max_adjusted = max(self.max_adjusted, number.adjusted())
+        self.min_exponent = min(self.min_exponent, ntuple.exponent)
+
+    def prepare(self):
+        digits_sign = 1 if self.has_negative else 0
+        digits_integral = max(self.max_adjusted, 0) + 1
+        digits_fractional = -self.min_exponent
+        digits_period = 1 if digits_fractional > 0 else 0
+        width = digits_sign + digits_integral + digits_period + digits_fractional
+        self.total_width = width
+        self.fmt = '{{:{sign}{width:d}.{precision:d}f}}'.format(
+            sign=' ' if digits_sign > 0 else '',
+            width=width,
+            precision=digits_fractional)
+        self.empty = ' ' * width
+
+    def width(self):
+        return self.total_width
+
+    def format(self, number):
+        return self.empty if number is None else self.fmt.format(number)
+
+
+class PositionRenderer(ColumnRenderer):
+    """A renderer for positions. Inventories renders as a list of position
+    strings. Both the unit numbers and the cost numbers are aligned, if any.
+    """
+    dtype = position.Position
+
+    def __init__(self):
+        super().__init__()
+        self.units_rdr = DecimalRenderer()
+        self.units_ccylen = 0
+        self.cost_rdr = DecimalRenderer()
+        self.cost_ccylen = 0
+
+    def update(self, pos):
+        if pos is None:
+            return
+        lot = pos.lot
+        cost = lot.cost
+        self.units_rdr.update(pos.number)
+        self.units_ccylen = max(self.units_ccylen, len(lot.currency))
+        if cost:
+            self.cost_rdr.update(cost.number)
+            self.cost_ccylen = max(self.cost_ccylen, len(cost.currency))
+
+    def prepare(self):
+        self.units_rdr.prepare()
+        self.cost_rdr.prepare()
+
+        fmt_units = '{{:{0}}} {{:{1}}}'.format(self.units_rdr.width(), self.units_ccylen)
+        fmt_cost = '{{{{{{:{0}}} {{:{1}}}}}}}'.format(self.cost_rdr.width(), self.cost_ccylen)
+
+        if self.cost_ccylen == 0:
+            self.fmt_cost = None # Will not get used.
+            self.fmt_nocost = fmt_units
+        else:
+            self.fmt_cost = '{} {}'.format(fmt_units, fmt_cost)
+            self.fmt_nocost = '{} {}'.format(fmt_units, ' ' * len(fmt_cost.format('', '')))
+
+        self.empty = self.fmt_nocost.format('', '')
+
+    def width(self):
+        return len(self.empty)
+
+    def format(self, pos):
+        strings = []
+        if self.cost_ccylen == 0:
+            lot = pos.lot
+            strings.append(
+                self.fmt_nocost.format(
+                    self.units_rdr.format(pos.number),
+                    lot.currency))
+
+        else:
+            lot = pos.lot
+            cost = lot.cost
+            if cost:
+                strings.append(
+                    self.fmt_cost.format(
+                        self.units_rdr.format(pos.number),
+                        lot.currency,
+                        self.cost_rdr.format(cost.number if cost else None),
+                        cost.currency if cost else ''))
+            else:
+                strings.append(
+                    self.fmt_nocost.format(
+                        self.units_rdr.format(pos.number),
+                        lot.currency))
+
+        if len(strings) == 1:
+            return strings[0]
+        elif len(strings) == 0:
+            return self.empty
+        else:
+            return strings
+
+
+class InventoryRenderer(PositionRenderer):
+    """A renderer for Inventoru instances. Inventories renders as a list of position
+    strings. Both the unit numbers and the cost numbers are aligned, if any.
+    """
+    dtype = inventory.Inventory
+
+    def update(self, inv):
+        if inv is None:
+            return
+        for pos in inv.get_positions():
+            super().update(pos)
+
+    def format(self, inv):
+        strings = []
+        if self.cost_ccylen == 0:
+            for position in inv.get_positions():
+                lot = position.lot
+                strings.append(
+                    self.fmt_nocost.format(
+                        self.units_rdr.format(position.number),
+                        lot.currency))
+
+        else:
+            for position in inv.get_positions():
+                lot = position.lot
+                cost = lot.cost
+                if cost:
+                    strings.append(
+                        self.fmt_cost.format(
+                            self.units_rdr.format(position.number),
+                            lot.currency,
+                            self.cost_rdr.format(cost.number if cost else None),
+                            cost.currency if cost else ''))
+                else:
+                    strings.append(
+                        self.fmt_nocost.format(
+                            self.units_rdr.format(position.number),
+                            lot.currency))
+
+        if len(strings) == 1:
+            return strings[0]
+        elif len(strings) == 0:
+            return self.empty
+        else:
+            return strings
+
+
+def get_renderers(result_types, result_rows):
+    # Create renderers for each column.
+    renderers = [RENDERERS[dtype]() for _, dtype in result_types]
+
+    # Prime and prepare each of the renderers with the date in order to be ready
+    # to begin rendering with correct alignment.
+    for row in result_rows:
+        for value, renderer in zip(row, renderers):
+            renderer.update(value)
+
+    for renderer in renderers:
+        renderer.prepare()
+
+    return renderers
 
 
 def render_text(result_types, result_rows, file):
@@ -35,23 +299,16 @@ def render_text(result_types, result_rows, file):
     #   formats in order to be importable in a spreadsheet in a way that numbers
     #   are usable.
 
-    # Get the names of the columns.
+    if result_rows:
+        assert len(result_types) == len(result_rows[0])
+
+    # # Get the names of the columns.
     num_cols = len(result_types)
-    col_names = [name for name, _ in result_types]
+    # col_names = [name for name, _ in result_types]
 
-    # Figure out functions to render each column to to a stirng.
-    col_funcs = []
-    col_align = []
-    for name, dtype in result_types:
-        render_func, render_format = RENDERERS[dtype]
-        col_funcs.append(render_func)
-        col_align.append(render_format)
+    renderers = get_renderers(result_types, result_rows)
 
-    # Keep track of the maximum width required to render each column.
-    col_widths = [0 for _ in range(num_cols)]
-
-    # Render all the columns of all the rows to strings and compute their width
-    # at the same time.
+    # Render all the columns of all the rows to strings.
     str_rows = []
     for row in result_rows:
         # Rendering each row involves rendering all the columns, each of which
@@ -63,65 +320,61 @@ def render_text(result_types, result_rows, file):
         # strings. This routine also computes the maximum number of rows that a
         # rendered value will generate.
         exp_row = []
-        num_lines = 1
-        for value, render_func in zip(row, col_funcs):
-            exp_value = render_func(value)
-            if isinstance(exp_value, list):
-                if len(exp_value) == 1:
-                    exp_value = exp_value[0]
-                elif len(exp_value) == 0:
-                    exp_value = ''
-                else:
-                    num_lines = max(num_lines, len(exp_value))
-            exp_row.append(exp_value)
+        max_lines = 1
+        for value, renderer in zip(row, renderers):
+            # Update the column renderer.
+            exp_lines = renderer.format(value)
+            if isinstance(exp_lines, list):
+                max_lines = max(max_lines, len(exp_lines))
+            exp_row.append(exp_lines)
 
         # If all the values were rendered directly to strings, this is row
         # renders on a single line. Just append this one row. This is the common
         # case.
-        if num_lines == 1:
+        if max_lines == 1:
             str_rows.append(exp_row)
-            for col, exp_value in enumerate(exp_row):
-                col_widths[col] = max(col_widths[col], len(exp_value))
+
+        # Some of the values rendered to more than one line; we need to render
+        # them on separate lines and insert filler.
         else:
-            # Some of the values rendered to more than one line; we need to
-            # render them on separate lines and insert filler.
+            # Make sure all values in the column are wrapped in sequences.
             exp_row = [exp_value if isinstance(exp_value, list) else (exp_value,)
                        for exp_value in exp_row]
-            str_lines = [[] for _ in range(num_lines)]
-            for col, exp_value in enumerate(exp_row):
-                for index, str_line in zip_longest(range(num_lines), exp_value,
+
+            # Create a matrix of the column.
+            str_lines = [[] for _ in range(max_lines)]
+            for exp_value in exp_row:
+                for index, exp_line in zip_longest(range(max_lines), exp_value,
                                                    fillvalue=''):
-                    str_lines[index].append(str_line)
-                    if str_line:
-                        col_widths[col] = max(col_widths[col], len(str_line))
+                    str_lines[index].append(exp_line)
             str_rows.extend(str_lines)
 
     # Compute a final format string.
-    formats = []
-    for width, align in zip(col_widths, col_align):
-        formats.append('{{:{}{}.{}}}'.format(align, width, width))
+    formats = ['{{:{}}}'.format(renderer.width()) for renderer in renderers]
     line_formatter = '| ' + ' | '.join(formats) + ' |\n'
+    line_body = '-' + '-+-'.join(('-' * len(fmt.format(''))) for fmt in formats) + '-'
+    top_line = ",{}.\n".format(line_body)
+    middle_line = "+{}+\n".format(line_body)
+    bottom_line = "`{}'\n".format(line_body)
 
     # Render each string row to a single line.
+    file.write(top_line)
+    file.write(middle_line)
     for str_row in str_rows:
         line = line_formatter.format(*str_row)
         file.write(line)
+    file.write(bottom_line)
 
-
-identity = lambda x: x
-
-def render_inventory(inventory):
-    return [str(position_) for position_ in inventory.get_positions()]
 
 # A mapping of data-type -> (render-function, alignment)
-RENDERERS = {
-    str                 : (identity, '<'),
-    Decimal             : ('{:.2f}'.format, '>'),
-    inventory.Inventory : (render_inventory, '>'),
-    datetime.date       : (str, '<'),
-    }
-
-
+RENDERERS = {renderer_cls.dtype: renderer_cls
+             for renderer_cls in [
+                     StringRenderer,
+                     DecimalRenderer,
+                     DateTimeRenderer,
+                     PositionRenderer,
+                     InventoryRenderer,
+                 ]}
 
 
 def render_text__old(result_types, result_rows, file):
@@ -129,12 +382,8 @@ def render_text__old(result_types, result_rows, file):
     table.render_table(table_, file, 'text')
 
 
-# FIXME: We need to figure out rendering precision correctly. In order to do
-# this, process all the entries and look at the maximum precision used. You
-# should be able to compute a dictionary of {commodity: precision} from the list of entries. This should be tested independently.
+# FIXME: Create a StringSetRenderer
 
-# FIXME: Create formatter objects for each row type.
-#
 # FIXME: You need to render the header.
 #
 # FIXME: Check out if it's possible to precompile a format string for execution
--- src/python/beancount/query/query_render_test.py
+++ src/python/beancount/query/query_render_test.py
@@ -8,10 +8,121 @@ import collections
 
 from beancount.core.amount import D
 from beancount.core.amount import Decimal
+from beancount.core.amount import ZERO
 from beancount.core import inventory
 from beancount.core import position
 from beancount.query import query_render
 from beancount.utils.misc_utils import box
+
+
+class ColumnRendererBase(unittest.TestCase):
+
+    RendererClass = None
+
+    def get(self, *values):
+        rdr = self.RendererClass()
+        for value in values:
+            rdr.update(value)
+        rdr.prepare()
+        return rdr
+
+
+class TestStringRenderer(ColumnRendererBase):
+
+    RendererClass = query_render.StringRenderer
+
+    def test_simple(self):
+        rdr = self.get('a', 'bb', 'ccc', '')
+        self.assertEqual('dd ', rdr.format('dd'))
+        self.assertEqual('   ', rdr.format(''))
+
+    def test_nones(self):
+        rdr = self.get(None, 'bb', 'ccc', None)
+        self.assertEqual('dd ', rdr.format('dd'))
+        self.assertEqual('   ', rdr.format(None))
+
+    def test_overflow(self):
+        rdr = self.get('a', 'bb', 'ccc', '')
+        self.assertEqual('eee', rdr.format('eeee'))
+
+
+class TestDateTimeRenderer(ColumnRendererBase):
+
+    RendererClass = query_render.DateTimeRenderer
+
+    def test_simple(self):
+        rdr = self.get(datetime.date(2014, 11, 3))
+        self.assertEqual('2014-10-03', rdr.format(datetime.date(2014, 10, 3)))
+
+    def test_nones(self):
+        rdr = self.get(None, datetime.date(2014, 11, 3), None)
+        self.assertEqual('2014-03-30', rdr.format(datetime.date(2014, 3, 30)))
+        self.assertEqual('          ', rdr.format(None))
+
+
+class TestDecimalRenderer(ColumnRendererBase):
+
+    RendererClass = query_render.DecimalRenderer
+
+    def test_integers(self):
+        rdr = self.get(D('1'), D('222'), D('33'))
+        self.assertEqual('444', rdr.format(D('444')))
+
+    def test_fractional(self):
+        rdr = self.get(D('1.23'), D('1.2345'), D('2.345'))
+        self.assertEqual('1.0000', rdr.format(D('1')))
+        self.assertEqual('2.3457', rdr.format(D('2.34567890')))
+
+    def test_mixed(self):
+        rdr = self.get(D('1000'), D('0.12334'))
+        self.assertEqual('   1.00000', rdr.format(D('1')))
+
+    def test_zero_integers(self):
+        rdr = self.get(D('0.1234'))
+        self.assertEqual('1.0000', rdr.format(D('1')))
+
+    def test_nones(self):
+        rdr = self.get(None, D('0.1234'), None)
+        self.assertEqual('1.0000', rdr.format(D('1')))
+        self.assertEqual('      ', rdr.format(None))
+
+
+class TestInventoryRenderer(ColumnRendererBase):
+
+    RendererClass = query_render.InventoryRenderer
+
+    def test_various(self):
+        inv = inventory.from_string('100.00 USD')
+        rdr = self.get(inv)
+        self.assertEqual(['100.00 USD'],
+                         rdr.format(inv))
+
+        inv = inventory.from_string('5 GOOG {500.23 USD}')
+        rdr = self.get(inv)
+        self.assertEqual(['5 GOOG {500.23 USD}'],
+                         rdr.format(inv))
+
+        inv = inventory.from_string('5 GOOG {500.23 USD}, 12.3456 CAAD')
+        rdr = self.get(inv)
+        self.assertEqual([' 5.0000 GOOG {500.23 USD}',
+                          '12.3456 CAAD             '],
+                         rdr.format(inv))
+
+
+class TestPositionRenderer(ColumnRendererBase):
+
+    RendererClass = query_render.PositionRenderer
+
+    def test_various(self):
+        pos = position.from_string('100.00 USD')
+        rdr = self.get(pos)
+        self.assertEqual(['100.00 USD'],
+                         rdr.format(pos))
+
+        pos = position.from_string('5 GOOG {500.23 USD}')
+        rdr = self.get(pos)
+        self.assertEqual(['5 GOOG {500.23 USD}'],
+                         rdr.format(pos))
 
 
 class TestQueryRender(unittest.TestCase):
