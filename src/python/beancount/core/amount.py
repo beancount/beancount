75b4915c40b6ef3fecb2460f7a49af191e4304b4
Martin Blais <blais@furius.ca>
2014-11-11 13:06:55 -0500

    Created a DisplayContext object to contain and provide the precision and formatting globally.

--- src/python/beancount/core/amount.py
+++ src/python/beancount/core/amount.py
@@ -269,3 +269,91 @@ def amount_sub(amount1, amount2):
 
 from_string = Amount.from_string
 NULL_AMOUNT = Amount(ZERO, '')
+
+
+class DisplayContext:
+    """A class used to contain various settings that control how we output numbers.
+    In particular, the precision used for each currency, and whether or not
+    commas should be printed. This object is intended to be passed around to all
+    functions that format numbers to strings.
+
+    Attributes:
+      commas: A boolean, whether we should render commas or not.
+      precision: A dict of currency to precision. The value for a key of None
+        provides the default precision.
+      formats: A dict of currency to a pre-baked format string to render a
+        number. (A key of None is treated as for self.precision.)
+      precision_max: Like 'precision' but for maximum number of digits.
+      formats_max: Like 'formats' but for maximum number of digits.
+      default_format: The default display format.
+    """
+
+    # The default precision for all unset values.
+    default_precision = 2
+
+    def __init__(self):
+        self.commas = False
+        self.precision = {None: self.default_precision}
+        self.precision_max = {None: self.default_precision}
+        self.formats = {}
+        self.update()
+
+    @staticmethod
+    def _formats_dict(precision_dict, commas):
+        comma_str = ',' if commas else ''
+        formats = {}
+        for currency, precision in precision_dict.items():
+            formats[currency] = (
+                '{{: {0}.{1}f}}'.format(comma_str, precision))
+        default_format = formats[None]
+        return formats, default_format
+
+    def update(self):
+        self.formats, self.formats_default = (
+            self._formats_dict(self.precision, self.commas))
+        self.formats_max, self.formats_max_default = (
+            self._formats_dict(self.precision_max, self.commas))
+
+    def set_commas(self, commas, update=True):
+        self.commas = commas
+        if update:
+            self.update()
+
+    def set_precision(self, precision, currency=None, update=True):
+        assert isinstance(precision, int), precision
+        self.precision[currency] = precision
+        if update:
+            self.update()
+
+    def set_precision_max(self, precision, currency=None, update=True):
+        assert isinstance(precision, int)
+        self.precision_max[currency] = precision
+        if update:
+            self.update()
+
+    def format(self, number, currency=None):
+        fmt = self.formats.get(currency, self.formats_default)
+        return fmt.format(number)
+
+    def format_max(self, number, currency=None):
+        fmt = self.formats_max.get(currency, self.formats_max_default)
+        return fmt.format(number)
+
+    __call__ = format
+
+    def dump(self, file):
+        max_currency_width = max(len(ccy) if ccy is not None else 1
+                                 for ccy in self.precision)
+        fmt = '{{:{}}}   {{:2}} | {{:24}}   {{:2}} | {{:24}}'.format(max_currency_width)
+        currencies = set(self.precision) | set(self.precision_max)
+        currencies.discard(None)
+        for currency in [None] + sorted(currencies):
+            precision = self.precision.get(currency, '')
+            precision_max = self.precision_max.get(currency, '')
+            sample = self.formats.get(currency, '').format(0)
+            sample_max = self.formats_max.get(currency, '').format(0)
+            # if precision_max == precision and formats == formats_max:
+            #     precision_max = ''
+            #     formats_max = ''
+            print(fmt.format(currency or '*', precision, sample, precision_max, sample_max),
+                  file=file)
--- src/python/beancount/core/amount_test.py
+++ src/python/beancount/core/amount_test.py
@@ -2,6 +2,7 @@ __author__ = "Martin Blais <blais@furius.ca>"
 
 import unittest
 import re
+import io
 
 from .amount import D
 from .amount import Decimal
@@ -170,3 +171,59 @@ class TestAmount(unittest.TestCase):
         with self.assertRaises(ValueError):
             amount.amount_sub(Amount('100', 'USD'),
                               Amount('17.02', 'CAD'))
+
+
+class TestDisplayPrecision(unittest.TestCase):
+
+    number = D('-1764.9876543')
+
+    def test_precision_empty_default(self):
+        dc = amount.DisplayContext()
+        self.assertEqual('-1764.99', dc.format(self.number))
+        self.assertEqual('-1764.99', dc(self.number))
+
+    def test_precision_commas(self):
+        dc = amount.DisplayContext()
+        dc.set_commas(True)
+        self.assertEqual('-1,764.99', dc.format(self.number))
+        dc.set_commas(False)
+        self.assertEqual('-1764.99', dc.format(self.number))
+
+    def test_precision_default(self):
+        dc = amount.DisplayContext()
+        dc.set_precision(4)
+        self.assertEqual('-1764.9877', dc.format(self.number))
+        dc.set_precision(3)
+        self.assertEqual('-1764.988', dc.format(self.number))
+
+    def test_precision_currency(self):
+        dc = amount.DisplayContext()
+        dc.set_precision(5, 'RBF1010')
+        self.assertEqual('-1764.99', dc.format(self.number))
+        self.assertEqual('-1764.98765', dc.format(self.number, 'RBF1010'))
+        dc.set_precision(1)
+        dc.set_precision(3, 'RBF1010')
+        self.assertEqual('-1765.0', dc.format(self.number))
+        self.assertEqual('-1764.988', dc.format(self.number, 'RBF1010'))
+
+    def test_precision_integer(self):
+        dc = amount.DisplayContext()
+        dc.set_precision(0)
+        self.assertEqual('-1765', dc.format(self.number))
+
+    def test_precision_max(self):
+        dc = amount.DisplayContext()
+        dc.set_precision(2)
+        dc.set_precision_max(4)
+        self.assertEqual('-1764.99', dc.format(self.number))
+        self.assertEqual('-1764.9877', dc.format_max(self.number))
+
+    def test_precision_dump(self):
+        dc = amount.DisplayContext()
+        dc.set_precision(2)
+        dc.set_precision(2, 'CAD')
+        dc.set_precision_max(8, 'CAD')
+        dc.set_precision(4, 'RBF1010')
+        oss = io.StringIO()
+        dc.dump(oss)
+        self.assertTrue(oss.getvalue())
