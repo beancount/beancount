1c3e71d81c46631f17d4c69d658041400d077e0c
Martin Blais <blais@furius.ca>
2015-04-04 14:50:17 -0400

    Better tests for sellgains check.

--- src/python/beancount/plugins/sellgains_test.py
+++ src/python/beancount/plugins/sellgains_test.py
@@ -3,7 +3,8 @@ __author__ = "Martin Blais <blais@furius.ca>"
 import unittest
 
 from beancount import loader
-from beancount.plugins import check_commodity
+from beancount.plugins import sellgains
+from beancount.ops import validation
 from beancount.parser import printer
 
 
@@ -21,10 +22,11 @@ class TestSellGains(unittest.TestCase):
           Expenses:Financial:Fees        0.08 USD
           Income:US:Company:ESPP:PnL
         """
+        printer.print_errors(errors)
         self.assertEqual([], errors)
 
     @loader.loaddoc
-    def test_sellgains_fail(self, entries, errors, options_map):
+    def test_sellgains_fail_balance(self, entries, errors, options_map):
         """
         option "plugin" "beancount.ops.auto_accounts"
         option "plugin" "beancount.plugins.sellgains"
@@ -32,6 +34,21 @@ class TestSellGains(unittest.TestCase):
         1999-07-31 * "Sell"
           Assets:US:Company:ESPP          -81 ADSK {26.3125 USD} @ 26.4375 USD
           Assets:US:Company:Cash      2141.36 USD
-          Income:US:Company:ESPP:PnL
+          Expenses:Financial:Fees        1.08 USD
+          Income:US:Company:ESPP:PnL   -11.13 USD
         """
-        self.assertTrue(errors)
+        self.assertEqual([sellgains.SellGainsError], list(map(type, errors)))
+
+    @loader.loaddoc
+    def test_sellgains_fail_imbalance(self, entries, errors, options_map):
+        """
+        option "plugin" "beancount.ops.auto_accounts"
+        option "plugin" "beancount.plugins.sellgains"
+
+        1999-07-31 * "Sell"
+          Assets:US:Company:ESPP          -81 ADSK {26.3125 USD} @ 26.4375 USD
+          Assets:US:Company:Cash      2141.36 USD
+          Income:US:Company:ESPP:PnL   -11.13 USD
+        """
+        self.assertEqual([sellgains.SellGainsError,
+                          validation.ValidationError], list(map(type, errors)))
