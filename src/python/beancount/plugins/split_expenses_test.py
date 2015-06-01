cc8b44c8b716e5bac96d7fab601561d8f100db1c
Martin Blais <blais@furius.ca>
2015-05-31 20:05:13 -0400

    Implemented more tests for beancount.plugins.split_expenses.

--- src/python/beancount/plugins/split_expenses_test.py
+++ src/python/beancount/plugins/split_expenses_test.py
@@ -1,5 +1,6 @@
 __author__ = "Martin Blais <blais@furius.ca>"
 
+import textwrap
 import unittest
 
 from beancount import loader
@@ -13,7 +14,6 @@ class TestSplitExpenses(cmptest.TestCase):
     @loader.loaddoc
     def test_simple(self, entries, errors, __):
         """
-            plugin "beancount.ops.auto_accounts"
             plugin "beancount.plugins.split_expenses" "Martin Caroline"
 
             2011-05-17 * "Something"
@@ -21,14 +21,33 @@ class TestSplitExpenses(cmptest.TestCase):
               Assets:Cash          -2.00 USD
         """
         self.assertEqualEntries("""
+
+            ;; Added automatically by the plugin.
+            2011-05-17 open Expenses:Restaurant:Caroline
+            2011-05-17 open Expenses:Restaurant:Martin
+
             2011-05-17 * "Something"
               Expenses:Restaurant:Martin     1.00 USD
               Expenses:Restaurant:Caroline   1.00 USD
               Assets:Cash                   -2.00 USD
-        """, [entries[-1]])
+
+        """, entries)
 
     @loader.loaddoc
     def test_unaffected(self, entries, errors, __):
+        """
+            plugin "beancount.plugins.split_expenses" "Martin Caroline"
+
+            2011-05-17 * "Something"
+              Expenses:Restaurant:Martin   2.00 USD
+              Assets:Cash                 -2.00 USD
+        """
+        # Note that this checks that the plugin did not insert any Open
+        # directive by itself where not required. This is correct behaviour.
+        self.assertEqualEntries(self.test_unaffected.__input__, entries)
+
+    @loader.loaddoc
+    def test_work_with_auto_accounts(self, entries, errors, __):
         """
             plugin "beancount.ops.auto_accounts"
             plugin "beancount.plugins.split_expenses" "Martin Caroline"
@@ -37,8 +56,48 @@ class TestSplitExpenses(cmptest.TestCase):
               Expenses:Restaurant:Martin   2.00 USD
               Assets:Cash                 -2.00 USD
         """
+        expected_input = textwrap.dedent("""
+            2011-05-17 open Assets:Cash
+            2011-05-17 open Expenses:Restaurant:Martin
+        """) + self.test_work_with_auto_accounts.__input__
+        self.assertEqualEntries(expected_input, entries)
+
+    @loader.loaddoc
+    def test_with_one_member_only(self, entries, errors, __):
+        """
+            plugin "beancount.plugins.split_expenses" "Martin"
+
+            2011-05-17 * "Something"
+              Expenses:Restaurant          2.00 USD
+              Assets:Cash                 -2.00 USD
+        """
         self.assertEqualEntries("""
+            2011-05-17 open Expenses:Restaurant:Martin
+
             2011-05-17 * "Something"
               Expenses:Restaurant:Martin   2.00 USD
               Assets:Cash                 -2.00 USD
-        """, [entries[-1]])
+        """, entries)
+
+    @loader.loaddoc
+    def test_other_directives_copied(self, entries, errors, __):
+        """
+            plugin "beancount.plugins.split_expenses" "Martin Caroline"
+
+            2011-01-01 open Expenses:Restaurant  USD
+            2011-01-01 open Assets:Cash
+
+            2011-05-17 * "Something"
+              Expenses:Restaurant   2.00 USD
+              Assets:Cash          -2.00 USD
+
+            2011-05-30 balance Expenses:Restaurant  2.00 USD
+            2011-06-01 close   Expenses:Restaurant
+        """
+        self.assertIncludesEntries("""
+            2011-01-01 open Expenses:Restaurant  USD
+            2011-01-01 open Assets:Cash
+
+            2011-05-30 balance Expenses:Restaurant  2.00 USD
+            2011-06-01 close   Expenses:Restaurant
+        """, entries)
