5d9b02cb29df5491d6f671e130b19d2c6c3fa551
Martin Blais <blais@furius.ca>
2015-07-12 15:48:42 -0400

    Fixed tests.

--- src/python/beancount/reports/tree_table_test.py
+++ src/python/beancount/reports/tree_table_test.py
@@ -6,7 +6,9 @@ import unittest
 
 from beancount import loader
 from beancount.core import realization
+from beancount.core import display_context
 from beancount.reports import tree_table
+from beancount.reports import html_formatter
 
 
 class TestActiveAccounts(unittest.TestCase):
@@ -70,7 +72,8 @@ class TestTables(unittest.TestCase):
         self.assertTrue(re.search('Assets:US:Checking', html))
 
     def test_table_of_balances(self):
-        html = tree_table.table_of_balances(self.real_root, ['USD', 'CAD'], None,
+        formatter = html_formatter.HTMLFormatter(display_context.DEFAULT_DISPLAY_CONTEXT)
+        html = tree_table.table_of_balances(self.real_root, ['USD', 'CAD'], formatter,
                                             classes=['586e8200b379'])
         self.assertTrue(re.search('<table', html))
         self.assertTrue(re.search('USD', html))
