028f2964d36779f58f3ffdd173d99705744b53ba
Martin Blais <blais@furius.ca>
2015-09-08 01:54:45 -0400

    Made currency order stable in numberification when multiple identical frequencies exist.

--- src/python/beancount/query/numberify_test.py
+++ src/python/beancount/query/numberify_test.py
@@ -75,3 +75,21 @@ class TestNumerifyIdentity(unittest.TestCase):
         atypes, arows = numberify.numberify_results(itypes, irows)
         self.assertEqual(itypes, atypes)
         self.assertEqual(irows, arows)
+
+
+class TestNumerifyInventory(unittest.TestCase):
+
+    def test_inventory(self):
+        itypes = [('balance', inventory.Inventory)]
+        irows = [[inventory.from_string('10 GOOG {23.00 USD}')],
+                 [inventory.from_string('2.11 USD, 3.44 CAD')],
+                 [inventory.from_string('-2 GOOG {24.00 USD}, 5.66 CAD')]]
+        atypes, arows = numberify.numberify_results(itypes, irows)
+
+        self.assertEqual([('balance (GOOG)', Decimal),
+                          ('balance (CAD)', Decimal),
+                          ('balance (USD)', Decimal)], atypes)
+
+        self.assertEqual([[D('10'), None, None],
+                          [None, D('3.44'), D('2.11')],
+                          [D('-2'), D('5.66'), None]], arows)
