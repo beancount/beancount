ece703e5e02242ceca86250e34df9a6e36ee5a9a
Martin Blais <blais@furius.ca>
2015-09-25 10:22:05 +0900

    Added more tests for FIFO plugin.

--- src/python/beancount/plugins/fifo.py
+++ src/python/beancount/plugins/fifo.py
@@ -1,48 +1,125 @@
-"""A kludge to hack FIFO bookings until Beancount actually has them supported.
+"""A plugin that automatically converts postings at price to postings held at
+cost, applying an automatic booking algorithm in assigning the cost bases and
+matching lots.
+
+This plugin restricts itself to applying these transformations within a
+particular account, which you provide. For each of those accounts, it also
+requires a corresponding Income account to book the profit/loss of reducing
+lots (i.e., sales):
+
+  plugin "beancount.plugins.fifo" "Assets:Bitcoin,Income:Bitcoin"
+
+Then, simply input the transactions with price conversion. We use "Bitcoins" in
+this example, converting Bitcoin purchases that were carried out as currency
+into maintaining these with cost basis, for tax reporting purposes:
+
+  2015-09-04 * "Buy some bitcoins"
+    Assets:Bank          -1000.00 USD
+    Assets:Bitcoin       4.333507 BTC @ 230.76 USD
+
+  2015-09-05 * "Buy some more bitcoins at a different price"
+    Assets:Bank          -1000.00 USD
+    Assets:Bitcoin       4.345747 BTC @ 230.11 USD
+
+  2015-09-20 * "Use (sell) some bitcoins"
+    Assets:Bitcoin       -6.000000 BTC @ 230.50 USD
+    Expenses:Something
+
+The result is that cost bases are inserted on augmenting lots:
+
+  2015-09-04 * "Buy some bitcoins"
+    Assets:Bitcoin  4.333507 BTC {230.76 USD} @ 230.76 USD
+    Assets:Bank     -1000.00 USD
+
+  2015-09-05 * "Buy some more bitcoins at a different price"
+    Assets:Bitcoin  4.345747 BTC {230.11 USD} @ 230.11 USD
+    Assets:Bank     -1000.00 USD
+
+While on reducing lots, matching FIFO lots are automatically found and the
+corresponding cost basis added:
+
+  2015-09-20 * "Use (sell) some bitcoins"
+    Assets:Bitcoin          -4.333507 BTC {230.76 USD} @ 230.50 USD
+    Assets:Bitcoin          -1.666493 BTC {230.11 USD} @ 230.50 USD
+    Income:Bitcoin         0.47677955 USD
+    Expenses:Something  1383.00000000 USD
+
+Note that multiple lots were required to fulfill the sale quantity here. As in
+this example, this may result in multiple lots being created for a single one.
+
+Finally, Beancount will eventually support booking methods built-in, but this is
+a quick method that shows how to hack your own booking method via
+transformations of the postings that run in in a plugin.
 """
 
 __author__ = 'Martin Blais <blais@furius.ca>'
-__plugins__ = ('rebook_as_fifo',)
+__plugins__ = ('book_price_conversions_as_fifo',)
+
+import copy
+import collections
+import logging
+import re
 
 from beancount.core.number import ZERO
 from beancount.core.number import D
+from beancount.core.amount import Amount
+from beancount.core.position import Position
 from beancount.core import data
-from beancount.core import amount
-from beancount.core import position
 from beancount.parser import printer
 
 
-CCY_FIFO = 'BTC'
-CCY_CASH = 'USD'
 
-
-def is_special(posting):
+def is_matching(posting, account):
     """"A special posting is one in the FIFO currency that doesn't have a cost and that
     has a price.
+
+    Args:
+      posting: An instance of a Posting.
+      account: The account name configured.
+    Returns:
+      A boolean, true if this posting is one that we should be concerned with.
     """
-    pos = posting.position
-    return (pos.lot.currency == CCY_FIFO and
-            pos.lot.cost is None and
+    return (posting.account == account and
+            posting.position.lot.cost is None and
             posting.price is not None)
 
 
-def rebook_as_fifo(entries, options_map):
-    new_entries = []
+# An error in the configuration for this plugin.
+ConfigError = collections.namedtuple('ConfigError', 'source message entry')
+
+# A failure to find a matching lot.
+FIFOError = collections.namedtuple('FIFOError', 'source message entry')
+
+
+def book_price_conversions_as_fifo(entries, options_map, config):
+    # The expected configuration is two account names, separated by whitespace.
     errors = []
+    try:
+        assets_account, income_account = re.split(r'[ \t,;]', config)
+    except Exception as exc:
+        errors.append(
+            ConfigError(None,
+                        ('Invalid configuration: "{}"; '
+                         'should be two accounts, skipping booking').format(config),
+                        None))
+        return entries, errors
+
+    # Pairs of (Position, Transaction) instances used to match augmenting
+    # entries with reducing ones.
+    fifo_lots = []
+
+    new_entries = []
     for entry in entries:
 
         # Figure out if this transaction has postings in Bitcoins without a cost.
         # The purpose of this plugin is to fixup those.
-        if (isinstance(entry, data.Transaction) and
-            any(is_special(posting) for posting in entry.postings)):
-
+        if isinstance(entry, data.Transaction) and any(is_matching(posting, assets_account)
+                                                       for posting in entry.postings):
             # Segregate the reducing lots, augmenting lots and other lots.
-            reducing = []
-            augmenting = []
-            other = []
+            augmenting, reducing, other = [], [], []
             for index, posting in enumerate(entry.postings):
-                if is_special(posting):
-                    out = reducing if posting.position.number < ZERO else augmenting
+                if is_matching(posting, assets_account):
+                    out = augmenting if posting.position.number >= ZERO else reducing
                 else:
                     out = other
                 out.append(posting)
@@ -52,24 +129,67 @@ def rebook_as_fifo(entries, options_map):
             # different lots.
             new_postings = []
 
-            # First, book the reducing postings as FIFO on the list of all
-            # accumulated Bitcoins.
+            # Convert all the augmenting postings to cost basis.
+            for posting in augmenting:
+                lot = posting.position.lot._replace(cost=copy.copy(posting.price))
+                pos = Position(lot, posting.position.number)
+                new_postings.append(posting._replace(position=pos))
+                fifo_lots.append( (copy.copy(pos), entry) )
+
+            # Then process all the reducing postings, booking them to FIFO basis.
+            pnl = ZERO
             for posting in reducing:
-                # FIXME: get cost amount... need to loop here.
-                new_lot = posting.position.lot._replace(
-                    cost=amount.Amount(cost, CCY_CASH))
-                new_postings.append(posting._replace(
-                    position=position.Position(new_lot, pos.number)))
+                match_number = -posting.position.number
+                match_currency = posting.position.lot.currency
+                cost_currency = posting.price.currency
+                while match_number != ZERO:
+                    # Find the first lot with matching currency.
+                    for fpos, fentry in fifo_lots:
+                        if (fpos.lot.currency == match_currency and
+                            fpos.lot.cost and fpos.lot.cost.currency == cost_currency):
+                            break
+                    else:
+                        errors.append(
+                            FIFOError(posting.meta,
+                                      "Could not match position {}".format(posting),
+                                      entry))
+                        break
 
-            # Second, compute the total difference between the prices and costs
-            # allocated, and add a new Income leg to account for it.
-            # FIXME: TODO
+                    # Reduce the FIFO lots.
+                    number = min(match_number, fpos.number)
+                    cost = fpos.lot.cost
+                    match_number -= number
+                    fpos.number -= number
+                    if fpos.number == ZERO:
+                        fifo_lots.pop(0)
 
-            # Third, add back all the othe legs in.
-            # FIXME: TODO
+                    # Add a corresponding posting.
+                    pos = Position(lot._replace(cost=copy.copy(cost)), -number)
+                    new_postings.append(posting._replace(position=pos))
+
+                    # Update the P/L.
+                    pnl += number * (posting.price.number - pos.lot.cost.number)
+
+            if reducing:
+                # If some reducing lots were seen in this transcation, insert an
+                # Income leg to absorb the P/L.
+                new_postings.append(
+                    data.Posting(income_account,
+                                 data.Position(data.Lot(cost_currency, None, None), -pnl),
+                                 None, None, None))
+
+            # Third, add back all the other unrelated legs in.
+            for posting in other:
+                new_postings.append(posting)
 
             entry = entry._replace(postings=new_postings)
 
         new_entries.append(entry)
 
     return new_entries, errors
+
+
+# FIXME: TODO
+# - Add links too
+# - Show how to report trades using links, running this file as a script.
+# - Move code in functions, add tests.
--- src/python/beancount/plugins/fifo_test.py
+++ src/python/beancount/plugins/fifo_test.py
@@ -5,28 +5,204 @@ from beancount.parser import cmptest
 from beancount.parser import printer
 
 
-class TestRebookFIFO(cmptest.TestCase):
+class TestbookFIFO(cmptest.TestCase):
 
     @loader.load_doc()
-    def test_exclude_tag(self, entries, errors, __):
+    def test_fifo_example(self, entries, errors, __):
         """
-            plugin "beancount.plugins.fifo"
+          plugin "beancount.plugins.fifo" "Assets:Bitcoin,Income:Bitcoin"
 
-            2015-01-01 open Assets:Bitcoin   "NONE"
-            2015-01-01 open Assets:Bank
-            2015-01-01 open Expenses:Something
+          2015-01-01 open Assets:Bitcoin
+          2015-01-01 open Income:Bitcoin
+          2015-01-01 open Assets:Bank
+          2015-01-01 open Expenses:Something
 
-            2015-09-04 *
-              Assets:Bank          -1000.00 USD
-              Assets:Bitcoin       4.333507 BTC {230.76 USD}
+          2015-09-04 * "Buy some bitcoins"
+            Assets:Bank          -1000.00 USD
+            Assets:Bitcoin       4.333507 BTC @ 230.76 USD
 
-            2015-09-05 *
-              Assets:Bank          -1000.00 USD
-              Assets:Bitcoin       4.345747 BTC {230.11 USD}
+          2015-09-05 * "Buy some more bitcoins"
+            Assets:Bank          -1000.00 USD
+            Assets:Bitcoin       4.345747 BTC @ 230.11 USD
 
-            2015-09-20 *
-              Assets:Bitcoin       -1.000000 BTC
-              Expenses:Something
+          2015-09-20 * "Use some bitcoins from two lots"
+            Assets:Bitcoin       -6.000000 BTC @ 230.50 USD
+            Expenses:Something
         """
-        print('-' * 120)
-        printer.print_entries(entries)
+
+        self.assertEqualEntries("""
+
+          2015-01-01 open Assets:Bitcoin
+          2015-01-01 open Income:Bitcoin
+          2015-01-01 open Assets:Bank
+          2015-01-01 open Expenses:Something
+
+          2015-09-04 * "Buy some bitcoins"
+            Assets:Bitcoin  4.333507 BTC {230.76 USD} @ 230.76 USD
+            Assets:Bank     -1000.00 USD
+
+          2015-09-05 * "Buy some more bitcoins"
+            Assets:Bitcoin  4.345747 BTC {230.11 USD} @ 230.11 USD
+            Assets:Bank     -1000.00 USD
+
+          2015-09-20 * "Use some bitcoins from two lots"
+            Assets:Bitcoin          -4.333507 BTC {230.76 USD} @ 230.50 USD
+            Assets:Bitcoin          -1.666493 BTC {230.11 USD} @ 230.50 USD
+            Income:Bitcoin         0.47677955 USD
+            Expenses:Something  1383.00000000 USD
+
+        """, entries)
+
+    @loader.load_doc()
+    def test_fifo_split_augmenting(self, entries, errors, __):
+        """
+          plugin "beancount.plugins.fifo" "Assets:Bitcoin,Income:Bitcoin"
+
+          2015-01-01 open Assets:Bitcoin
+          2015-01-01 open Income:Bitcoin
+          2015-01-01 open Assets:Bank
+          2015-01-01 open Expenses:Something
+
+          2015-09-04 *
+            Assets:Bank          -1000.00 USD
+            Assets:Bitcoin       4.347826 BTC @ 230.00 USD
+
+          2015-09-20 *
+            Assets:Bitcoin       -2.000000 BTC @ 231.00 USD
+            Expenses:Something
+
+          2015-09-21 *
+            Assets:Bitcoin       -2.000000 BTC @ 232.00 USD
+            Expenses:Something
+
+          2015-09-22 *
+            Assets:Bitcoin       -0.347826 BTC @ 233.00 USD
+            Expenses:Something
+
+        """
+        self.assertEqualEntries("""
+
+          2015-01-01 open Assets:Bitcoin
+          2015-01-01 open Income:Bitcoin
+          2015-01-01 open Assets:Bank
+          2015-01-01 open Expenses:Something
+
+          2015-09-04 *
+            Assets:Bitcoin  4.347826 BTC {230.00 USD} @ 230.00 USD
+            Assets:Bank     -1000.00 USD
+
+          2015-09-20 *
+            Assets:Bitcoin         -2.000000 BTC {230.00 USD} @ 231.00 USD
+            Income:Bitcoin       -2.00000000 USD
+            Expenses:Something  462.00000000 USD
+
+          2015-09-21 *
+            Assets:Bitcoin         -2.000000 BTC {230.00 USD} @ 232.00 USD
+            Income:Bitcoin       -4.00000000 USD
+            Expenses:Something  464.00000000 USD
+
+          2015-09-22 *
+            Assets:Bitcoin        -0.347826 BTC {230.00 USD} @ 233.00 USD
+            Income:Bitcoin      -1.04347800 USD
+            Expenses:Something  81.04345800 USD
+
+        """, entries)
+
+    @loader.load_doc()
+    def test_fifo_split_reducing(self, entries, errors, __):
+        """
+          plugin "beancount.plugins.fifo" "Assets:Bitcoin,Income:Bitcoin"
+
+          2015-01-01 open Assets:Bitcoin
+          2015-01-01 open Income:Bitcoin
+          2015-01-01 open Assets:Bank
+          2015-01-01 open Expenses:Something
+
+          2015-09-04 *
+            Assets:Bank           -500.00 USD
+            Assets:Bitcoin       2.000000 BTC @ 250.00 USD
+
+          2015-09-05 *
+            Assets:Bank           -520.00 USD
+            Assets:Bitcoin       2.000000 BTC @ 260.00 USD
+
+          2015-09-06 *
+            Assets:Bank           -540.00 USD
+            Assets:Bitcoin       2.000000 BTC @ 270.00 USD
+
+          2015-09-20 *
+            Assets:Bitcoin       -5.000000 BTC @ 280.00 USD
+            Expenses:Something
+
+        """
+
+        self.assertEqualEntries("""
+
+          2015-01-01 open Assets:Bitcoin
+          2015-01-01 open Income:Bitcoin
+          2015-01-01 open Assets:Bank
+          2015-01-01 open Expenses:Something
+
+          2015-09-04 *
+            Assets:Bitcoin  2.000000 BTC {250.00 USD} @ 250.00 USD
+            Assets:Bank      -500.00 USD
+
+          2015-09-05 *
+            Assets:Bitcoin  2.000000 BTC {260.00 USD} @ 260.00 USD
+            Assets:Bank      -520.00 USD
+
+          2015-09-06 *
+            Assets:Bitcoin  2.000000 BTC {270.00 USD} @ 270.00 USD
+            Assets:Bank      -540.00 USD
+
+          2015-09-20 *
+            Assets:Bitcoin          -2.000000 BTC {250.00 USD} @ 280.00 USD
+            Assets:Bitcoin          -2.000000 BTC {260.00 USD} @ 280.00 USD
+            Assets:Bitcoin          -1.000000 BTC {270.00 USD} @ 280.00 USD
+            Income:Bitcoin      -110.00000000 USD
+            Expenses:Something  1400.00000000 USD
+
+        """, entries)
+
+    @loader.load_doc(expect_errors=True)
+    def test_fifo_split_partial_failure(self, entries, errors, __):
+        """
+          plugin "beancount.plugins.fifo" "Assets:Bitcoin,Income:Bitcoin"
+          plugin "beancount.plugins.auto_accounts"
+
+          2015-09-04 *
+            Assets:Bank           -500.00 USD
+            Assets:Bitcoin       2.000000 BTC @ 250.00 USD
+
+          2015-09-20 *
+            Assets:Bitcoin      -2.000001 BTC @ 280.00 USD
+            Expenses:Something
+        """
+        self.assertRegexpMatches(errors[0].message, "Could not match position")
+
+    @loader.load_doc(expect_errors=True)
+    def test_fifo_split_complete_failure(self, entries, errors, __):
+        """
+          plugin "beancount.plugins.fifo" "Assets:Bitcoin,Income:Bitcoin"
+          plugin "beancount.plugins.auto_accounts"
+
+          2015-09-04 *
+            Assets:Bank           -500.00 USD
+            Assets:Bitcoin       2.000000 BTC @ 250.00 USD
+
+          2015-09-20 *
+            Assets:Bitcoin      -2.000000 BTC @ 280.00 USD
+            Expenses:Something
+
+          2015-09-21 *
+            Assets:Bitcoin      -0.000001 BTC @ 280.00 USD
+            Expenses:Something
+        """
+        self.assertRegexpMatches(errors[0].message, "Could not match position")
+
+    @loader.load_doc(expect_errors=True)
+    def test_fifo_bad_configuration(self, entries, errors, __):
+        """
+          plugin "beancount.plugins.fifo" "Assets:Bitcoin"
+        """
+        self.assertRegexpMatches(errors[0].message, "Invalid configuration")
