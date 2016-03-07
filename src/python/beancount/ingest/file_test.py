ec6703fcb06bd13e34d736a3a972b5c03acfa296
Martin Blais <blais@furius.ca>
2016-03-06 16:28:01 -0800

    Refactored the insides of file() to test them better, and wrote unit tests for exceptions being raised from the file_*() methods.

--- src/python/beancount/ingest/file_test.py
+++ src/python/beancount/ingest/file_test.py
@@ -225,6 +225,58 @@ class TestScriptFile(scripts_utils.TestScriptsBase, test_utils.TestCase):
         self.assertEqual(0, error_mock.call_count)
         self.assertEqual(0, move_mock.call_count)
 
+    @mock.patch.object(logging, 'error')
+    def test_file__file_account_raises_exception(self, error_mock):
+        imp = mock.MagicMock()
+        imp.identify = mock.MagicMock(return_value=True)
+        exc = ValueError("Unexpected error!")
+        imp.file_account = mock.MagicMock(side_effect=exc)
+        imp.name = mock.MagicMock(return_value="SomeImporter")
+        file.file_one_file(path.join(self.downloads, 'ofxdownload.ofx'), [imp],
+                           self.documents)
+
+        self.assertEqual(2, error_mock.call_count)
+
+        args = error_mock.mock_calls[0][1]
+        self.assertRegex(args[0], 'raised an unexpected error')
+        self.assertEqual(args[1], 'SomeImporter')
+        self.assertEqual(args[2], exc)
+
+        args = error_mock.mock_calls[1][1]
+        self.assertRegex(args[0], 'No account provided by importers')
+
+    @mock.patch.object(logging, 'error')
+    def test_file__file_date_raises_exception(self, error_mock):
+        imp = mock.MagicMock()
+        imp.identify = mock.MagicMock(return_value=True)
+        exc = ValueError("Unexpected error!")
+        imp.file_date = mock.MagicMock(side_effect=exc)
+        imp.file_name = mock.MagicMock(side_effect='ofxdownload.ofx')
+        imp.name = mock.MagicMock(return_value="SomeImporter")
+        file.file_one_file(path.join(self.downloads, 'ofxdownload.ofx'), [imp],
+                           self.documents)
+        self.assertEqual(1, error_mock.call_count)
+        args = error_mock.mock_calls[0][1]
+        self.assertRegex(args[0], 'raised an unexpected error')
+        self.assertEqual(args[1], 'SomeImporter')
+        self.assertEqual(args[2], exc)
+
+    @mock.patch.object(logging, 'error')
+    def test_file__file_name_raises_exception(self, error_mock):
+        imp = mock.MagicMock()
+        imp.identify = mock.MagicMock(return_value=True)
+        exc = ValueError("Unexpected error!")
+        imp.file_date = mock.MagicMock(return_value=None)
+        imp.file_name = mock.MagicMock(side_effect=exc)
+        imp.name = mock.MagicMock(return_value="SomeImporter")
+        file.file_one_file(path.join(self.downloads, 'ofxdownload.ofx'), [imp],
+                           self.documents)
+        self.assertEqual(1, error_mock.call_count)
+        args = error_mock.mock_calls[0][1]
+        self.assertRegex(args[0], 'raised an unexpected error')
+        self.assertEqual(args[1], 'SomeImporter')
+        self.assertEqual(args[2], exc)
+
     def test_file(self):
         with test_utils.capture('stdout', 'stderr') as (stdout, stderr):
             test_utils.run_with_args(file.main, [
