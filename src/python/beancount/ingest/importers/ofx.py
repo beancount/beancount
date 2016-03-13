cd1fe38580ac6a5883e87b84b7d38af0a1986768
Martin Blais <blais@furius.ca>
2016-03-13 15:05:53 -0400

    Added Balance directive from the ofx importer.

--- src/python/beancount/ingest/importers/ofx.py
+++ src/python/beancount/ingest/importers/ofx.py
@@ -7,11 +7,13 @@ multiple times with different accounts if it has many accounts. It makes more
 sense to do it this way so that you can define your importer configuration
 account by account.
 
-Note that this importer is provided as an example and with no guarantees. If you
-need a more powerful or compliant OFX importer please consider either writing
-one or contributing changes. Also, this importer does its own very basic
-parsing; a better one would probably use (and depend on) the ofxparse module
-(see https://sites.google.com/site/ofxparse/).
+Note that this importer is provided as an example and with no guarantees. It's
+not really super great. On the other hand, I've been using it for more than five
+years over multiple accounts, so it has been useful to me (it works, by some
+measure of "works"). If you need a more powerful or compliant OFX importer
+please consider either writing one or contributing changes. Also, this importer
+does its own very basic parsing; a better one would probably use (and depend on)
+the ofxparse module (see https://sites.google.com/site/ofxparse/).
 """
 __author__ = "Martin Blais <blais@furius.ca>"
 
@@ -32,16 +34,16 @@ from beancount.ingest import importer
 class Importer(importer.ImporterProtocol):
     """An importer for Open Financial Exchange files."""
 
-    def __init__(self, acctid, account, basename=None):
+    def __init__(self, acctid_regexp, account, basename=None):
         """Create a new importer posting to the given account.
 
         Args:
           account: An account string, the account onto which to post all the
             amounts parsed.
-          acctid: A regexp, to match against the <ACCTID> tag of the OFX file.
+          acctid_regexp: A regexp, to match against the <ACCTID> tag of the OFX file.
           basename: An optional string, the name of the new files.
         """
-        self.acctid_regexp = re.compile(acctid)
+        self.acctid_regexp = acctid_regexp
         self.account = account
         self.basename = basename
 
@@ -73,34 +75,43 @@ class Importer(importer.ImporterProtocol):
 
     def extract(self, file):
         """Extract a list of partially complete transactions from the file."""
-        new_entries = []
+        soup = bs4.BeautifulSoup(file.contents(), 'lxml')
+        return extract(soup, file.name, self.acctid_regexp, self.account, self.FLAG)
+
+
+def extract(soup, filename, acctid_regexp, account, flag):
+    """Extract transactions from an OFX file.
+
+    Args:
+      soup: A BeautifulSoup root node.
+      acctid_regexp: A regular expression string matching the account we're interested in.
+      account: An account string onto which to post the amounts found in the file.
+      flag: A single-character string.
+    Returns:
+      A sorted list of entries.
+    """
+    new_entries = []
+    counter = itertools.count()
+    for acctid, currency, transactions, balance in find_statement_transactions(soup):
+        if not re.match(acctid_regexp, acctid):
+            continue
 
         # Create Transaction directives.
-        counter = itertools.count()
-        soup = bs4.BeautifulSoup(file.contents(), 'lxml')
-        for acctid, transactions in find_statement_transactions(soup):
-            for stmttrn in transactions:
-                entry = build_transaction(node, flag, account, currency)
-                entry.meta['filename'] = file.name
-                entry.meta['lineno'] = next(counter)
-                new_entries.append(entry)
+        for stmttrn in transactions:
+            entry = build_transaction(stmttrn, flag, account, currency)
+            entry = entry._replace(meta=data.new_metadata(filename, next(counter)))
+            new_entries.append(entry)
 
         # Create a Balance directive.
-        #     # Extract balance.
-        #     first_currency = find_currency(soup)
-        #     ledgerbal = soup.find('ledgerbal')
-        #     if ledgerbal:
-        #         balamt = find_child(ledgerbal, 'balamt', D)
-        #         dtasof = find_child(ledgerbal, 'dtasof', parse_ofx_time).date()
-        #         fileloc = data.new_metadata(file.name, next(txn_counter))
-        #         balance_entry = data.Balance(fileloc, dtasof, account_asset,
-        #                                      amount.Amount(balamt, first_currency),
-        #                                      None, None)
-        #         new_entries.append(balance_entry)
-        #
-        #     new_entries.sort(key=lambda entry: entry.date)
+        if balance:
+            date, number = balance
+            meta = data.new_metadata(filename, next(counter))
+            balance_entry = data.Balance(meta, date, account,
+                                         amount.Amount(number, currency),
+                                         None, None)
+            new_entries.append(balance_entry)
 
-        return new_entries
+    return data.sorted(new_entries)
 
 
 def parse_ofx_time(date_str):
@@ -161,8 +172,11 @@ def find_statement_transactions(soup):
     Args:
       soup: A BeautifulSoup root node.
     Yields:
-      A pair of an account id string and a list of transaction nodes (<STMTTRN>
-      BeautifulSoup tags).
+      A trip of
+        An account id string,
+        A currency string,
+        A list of transaction nodes (<STMTTRN> BeautifulSoup tags), and
+        A (date, balanace amount) for the <LEDGERBAL>.
     """
     # Process STMTTRNRS and CCSTMTTRNRS tags.
     for stmtrs in soup.find_all(re.compile('.*stmtrs$')):
@@ -174,9 +188,18 @@ def find_statement_transactions(soup):
             acctid_node = stmtrs.find('acctid')
             acctid = next(acctid_node.children).strip()
 
+            # Get the LEDGERBAL node. There appears to be a single one for all
+            # transaction lists.
+            ledgerbal = soup.find('ledgerbal')
+            balance = None
+            if ledgerbal:
+                dtasof = find_child(ledgerbal, 'dtasof', parse_ofx_time).date()
+                balamt = find_child(ledgerbal, 'balamt', D)
+                balance = (dtasof, balamt)
+
             # Process transaction lists (regular or credit-card).
             for tranlist in stmtrs.find_all(re.compile('(|bank|cc)tranlist')):
-                yield acctid, tranlist.find_all('stmttrn')
+                yield acctid, currency, tranlist.find_all('stmttrn'), balance
 
 
 def find_child(node, name, conversion=None):
