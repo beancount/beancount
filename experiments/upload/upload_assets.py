498d8d048b2000e3337231fa45de2b0a29051d43
Martin Blais <blais@furius.ca>
2016-05-22 08:39:13 -0400

    Exported tax field in upload script.

--- experiments/upload/upload_assets.py
+++ experiments/upload/upload_assets.py
@@ -91,10 +91,19 @@ def get_assets(entries, options_map):
 
 
 def add_prices_to_postings(entries, postings):
+    """Attach price directives to postings where missing.
+    The prices are fetched from the database of entries.
+
+    Args:
+      entries: A list of directives containing price directives.
+      postings: A list of Posting instances to be augmented.
+    Yields:
+      A new list of Posting instances with the 'price' attribute filled in.
+    """
     # Compute a price map, to extract most current prices.
     price_map = prices.build_price_map(entries)
     for posting in postings:
-        if posting.cost is not None:
+        if posting.price is None and posting.cost is not None:
             cbase = posting.units.currency
             cquote = posting.cost.currency
             (price_date,
@@ -122,11 +131,15 @@ def get_connection():
 class Model:
     """A model of the spreadsheet that makes it easy to configure the output."""
 
-    def __init__(self, postings, exports, asset_type, cash_currency='USD'):
+    def __init__(self, price_map, postings, exports, asset_type, tax_map,
+                 cash_currency='USD'):
+        self.price_map = price_map
+
         self.cash_currency = cash_currency
         self.postings = postings
         self.exports = exports
         self.asset_type = asset_type
+        self.tax_map = tax_map
 
         columns = [
             ('Account', self._account),
@@ -138,6 +151,7 @@ class Model:
             ('Unit Price', self._price_number),
             ('Conversion Factor', self._conversion),
             ('Asset Type', self._asset_type),
+            ('Taxation', self._taxation),
             # ('Label', self._cost_label),
             # ('Date', self._cost_date),
             # ('Price Currency', self._price_currency),
@@ -165,6 +179,8 @@ class Model:
 
     def _symbol(self, posting):
         symbol = self.exports.get(posting.units.currency, '')
+        # if symbol and ':' not in symbol:
+        #     logging.info("Posting: %-8s %s", symbol, posting)
         return ('' if symbol in ('CASH', 'IGNORE')
                 else symbol)
 
@@ -176,11 +192,11 @@ class Model:
 
     def _cost_number(self, posting):
         cost = posting.cost
-        return cost.number if cost is not None else ''
+        return cost.number if cost is not None else ONE
 
     def _cost_currency(self, posting):
         cost = posting.cost
-        return cost.currency if cost is not None else ''
+        return cost.currency if cost is not None else posting.units.currency
 
     def _cost_date(self, posting):
         date = posting.cost.date if posting.cost else None
@@ -191,7 +207,7 @@ class Model:
 
     def _price_number(self, posting):
         price = posting.price
-        return price.number if price is not None else ''
+        return price.number if price is not None else ONE
 
     def _price_currency(self, posting):
         price = posting.price
@@ -209,12 +225,23 @@ class Model:
 
         currency = self.CURRENCY_MAP.get(currency, currency)
         if currency == self.cash_currency:
-            return ONE
-        else:
+            # If its already in USD, a noop conversion is x1.
+            return str(ONE)
+        elif re.match('[A-Z]{3}$', currency):
+            # If the instrument is a currency instrument, fetch it live.
             return '=GOOGLEFINANCE("CURRENCY:{}{}")'.format(currency, self.cash_currency)
+        else:
+            # Otherwise, use the latest value in our file.
+            (_, price_number) = prices.get_price(self.price_map,
+                                                 (currency, self.cash_currency),
+                                                 None)
+            return str(price_number)
 
     def _asset_type(self, posting):
         return self.asset_type.get(posting.units.currency)
+
+    def _taxation(self, posting):
+        return self.tax_map.get(posting.account)
 
 
 def get_root_accounts(postings):
@@ -229,8 +256,7 @@ def get_root_accounts(postings):
       A dict of account names to root account names.
     """
     roots = {posting.account: (account.parent(posting.account)
-                               if (account.leaf(posting.account) ==
-                                   posting.units.currency)
+                               if account.leaf(posting.account) == posting.units.currency
                                else posting.account)
              for posting in postings}
     values = set(roots.values())
@@ -267,16 +293,43 @@ def aggregate_postings(postings):
         assert len(cost) == 1
         total_cost = cost[0].units
 
-        average_cost = position.Cost(total_cost.number/units.number,
-                                     total_cost.currency,
-                                     None, None)
+        if total_cost.currency != units.currency:
+            average_cost = position.Cost(total_cost.number/units.number,
+                                         total_cost.currency,
+                                         None, None)
+        else:
+            average_cost = None
         posting = data.Posting(account, units, average_cost, None, None, None)
+
         agg_postings.append(posting)
 
     return agg_postings
 
 
-def upload_postings_to_sheet(model, doc, index):
+def populate_with_parents(accounts_map, default):
+    """For each account key, propagate the values from their parent account.
+
+    Args:
+      tax_map: A dict of account name string to some value or None.
+      default: The default value to assign to those accounts for which we cannot resolve
+        a non-null value.
+    Returns:
+      An updated dict with None values of child accounts filled from the closest
+      values of their parent accounts.
+    """
+    new_accounts_map = accounts_map.copy()
+    for acc in accounts_map.keys():
+        curacc = acc
+        while curacc:
+            value = accounts_map.get(curacc, None)
+            if value is not None:
+                break
+            curacc = account.parent(curacc)
+        new_accounts_map[acc] = value or default
+    return new_accounts_map
+
+
+def upload_postings_to_sheet(model, doc, index, min_rows):
     """Upload a model to a spreadsheet.
 
     Args:
@@ -284,13 +337,25 @@ def upload_postings_to_sheet(model, doc, index):
       doc: A gspread.Spreadsheet instance.
       index: An index of a sheet in the spreadsheet.
     """
+    # Resize the sheet to the minimum number of required rows.
+    num_rows = max(min_rows, model.num_rows())
     sheet = doc.get_worksheet(index)
-    sheet.resize(model.num_rows(), model.num_cols())
-    cells = sheet.range(':'.join([sheet.get_addr_int(1, 1),
-                                  sheet.get_addr_int(model.num_rows(),
-                                                     model.num_cols())]))
+    sheet.resize(num_rows, model.num_cols())
+
+    # Fill up the data.
+    cells = sheet.range(':'.join([
+        sheet.get_addr_int(1, 1),
+        sheet.get_addr_int(model.num_rows(), model.num_cols())]))
     for cell in cells:
         cell.value = model.get(cell.row, cell.col)
+    sheet.update_cells(cells)
+
+    # Clear the remainder of the sheet.
+    cells = sheet.range(':'.join([
+        sheet.get_addr_int(model.num_rows()+1, 1),
+        sheet.get_addr_int(num_rows, model.num_cols())]))
+    for cell in cells:
+        cell.value = ''
     sheet.update_cells(cells)
 
 
@@ -299,6 +364,7 @@ def main():
     parser = argparse.ArgumentParser(description=__doc__.strip())
     parser.add_argument('filename', help='Beancount input file')
     parser.add_argument('docid', help="Spreadsheets doc id to update")
+    #parser.add_argument('-n', '--dry-run', action='store_true')
     args = parser.parse_args()
 
     # Connect to the API.
@@ -326,6 +392,7 @@ def main():
 
     # Aggregate postings by account/currency.
     agg_postings = sorted(aggregate_postings(postings), key=keyfun)
+    agg_postings = list(agg_postings)
 
     # Add prices to the postings.
     agg_postings = add_prices_to_postings(entries, agg_postings)
@@ -335,9 +402,23 @@ def main():
     exports = getters.get_values_meta(commodities_map, 'export')
     asset_type = getters.get_values_meta(commodities_map, 'assets')
 
-    # Create models and update the sheets.
-    upload_postings_to_sheet(Model(list(agg_postings), exports, asset_type), doc, 0)
-    #upload_postings_to_sheet(Model(postings, exports), doc, 0)
+    # Get the map of accounts to export meta tags.
+    accounts_map = {
+        account: open
+        for account, (open, _) in getters.get_account_open_close(entries).items()}
+    tax_map = populate_with_parents(getters.get_values_meta(accounts_map, 'tax'), 'TAXABLE')
+
+    # Filter out postings to be ignored.
+    agg_postings = [posting
+                    for posting in agg_postings
+                    if exports.get(posting.units.currency, None) != 'IGNORE']
+
+    # Realize the model.
+    price_map = prices.build_price_map(entries)
+    model = Model(price_map, list(agg_postings), exports, asset_type, tax_map)
+
+    # Update the sheets.
+    upload_postings_to_sheet(model, doc, 0, min_rows=100)
 
 
 if __name__ == '__main__':
