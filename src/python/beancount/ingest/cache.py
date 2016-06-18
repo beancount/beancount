d5b0b0399afb4a0c5dbac639a5b953560ff73d36
Martin Blais <blais@furius.ca>
2016-06-18 13:03:12 -0400

    Fixed issue #133, applied patch from Jean Yong.

--- src/python/beancount/ingest/cache.py
+++ src/python/beancount/ingest/cache.py
@@ -84,8 +84,11 @@ def head(num_bytes=8192):
       A converter function.
     """
     def head_reader(filename):
-        with open(filename) as file:
-            return file.read(num_bytes)
+        with open(filename, 'rb') as file:
+            rawdata = file.read(num_bytes)
+            detected = chardet.detect(rawdata)
+            encoding = detected['encoding']
+            return rawdata.decode(encoding)
     return head_reader
 
 
