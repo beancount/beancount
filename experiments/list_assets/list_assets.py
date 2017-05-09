59d9988ae6548b27f919a6adc81f0accd130f032
Martin Blais <blais@furius.ca>
2017-05-08 22:39:17 -0400

    Added comments to list_assets.

--- experiments/list_assets/list_assets.py
+++ experiments/list_assets/list_assets.py
@@ -20,13 +20,11 @@ __license__ = "GNU GPLv2"
 import argparse
 import copy
 import logging
-import re
 
 from google.protobuf import text_format
 
 from beancount import loader
 from beancount.parser import options
-from beancount.core.number import ZERO
 from beancount.core.number import D
 from beancount.core import account_types
 from beancount.core import realization
@@ -40,10 +38,16 @@ from beancount.utils import encryption
 from experiments.list_assets import assets_pb2
 
 
-Q = D('0.01')
-
-
 def prune_closed_accounts(real_root, ocmap):
+    """Prune closed accounts and their parents.
+
+    Args:
+      real_root: An instance of RealAccount.
+      ocmap: An open-close mapping.
+    Returns:
+      A pruned version of the RealAccount tree (the original one is left alone,
+      unmutated.
+    """
     real_result = copy.copy(real_root)
     real_result.clear()
 
@@ -64,7 +68,19 @@ def prune_closed_accounts(real_root, ocmap):
     return real_result
 
 
-def read_assets(filename, currency, reduce_accounts):
+def read_assets(filename, currency, reduce_accounts, quantization):
+    """Read a Beancount file and produce a list of assets.
+
+    Args:
+      filename: A string, the path to the Beancount file to read.
+      currency: A string, the currency to convert all the contents to.
+      reduce_accounts: A set of account names to be aggregated.
+      quantization: A Decimal instance, to quantize all the resulting amounts.
+    Returns:
+      A list of (account-name, number-balance), numbers being assumed to be in
+      the requested currency.
+    """
+
     # Read the Beancount input file.
     entries, _, options_map = loader.load_file(filename,
                                                log_errors=logging.error)
@@ -104,7 +120,7 @@ def read_assets(filename, currency, reduce_accounts):
         value_inv = real_acc.balance.reduce(lambda x: convert.get_value(x, price_map))
         currency_inv = value_inv.reduce(convert.convert_position, currency, price_map)
         amount = currency_inv.get_currency_units(currency)
-        accounts.append((real_acc.account, amount.number.quantize(Q)))
+        accounts.append((real_acc.account, amount.number.quantize(quantization)))
 
     # Reduce this list of (account-name, balance-number) sorted by reverse amount order.
     accounts.sort(key=lambda x: x[1], reverse=True)
@@ -112,6 +128,14 @@ def read_assets(filename, currency, reduce_accounts):
 
 
 def read_details(filename):
+    """Read the encrypted details file and create an index of its contents.
+
+    Args:
+      filename: A string, the name of the filename to read.
+    Returns:
+      A pair of the Details object containing the database and a mapping of
+      Beancount account name to its (institution, account) pair.
+    """
     # Read the encrypted details file.
     if encryption.is_encrypted_file(filename):
         contents = encryption.read_encrypted_file(filename)
@@ -126,15 +150,28 @@ def read_details(filename):
     # Create a mapping of account name to (institution, account) pairs.
     mapping = {}
     for institution in details.institution:
-        for account in institution.account:
-            assert account.beancount not in mapping, (
-                "Account name {} is not unique!".format(account.beancount))
-            mapping[account.beancount] = (institution, account)
+        for acc in institution.account:
+            assert acc.beancount not in mapping, (
+                "Account name {} is not unique!".format(acc.beancount))
+            mapping[acc.beancount] = (institution, acc)
 
     return details, mapping
 
 
 def find_parent(mapping, account_name):
+    """Find the deepest parent account present in a given dict or set.
+
+    Note that this begins and includes the given account name itself.
+
+    Args:
+      mapping: A dict or set instance.
+      account_name: A string, the name of an account.
+    Returns:
+      The first parent account name found.
+    Raises:
+      KeyError: If none of the parents of 'account_name' can be found in
+      'mapping.'
+    """
     while account_name:
         if account_name in mapping:
             return account_name
@@ -153,13 +190,17 @@ def main():
     parser.add_argument('-c', '--currency', action='store', default='USD',
                         help="The single currency to convert to for sorting")
 
+    parser.add_argument('-q', '--digits', action='store', default='0.01',
+                        help="An example price with the desired number of output digits")
+
     args = parser.parse_args()
 
     # Read the encrypted details file.
     details, mapping = read_details(args.details_filename)
 
     # Read the list of assets.
-    assets = read_assets(args.beancount_filename, args.currency, set(mapping))
+    quantization = D(args.digits)
+    assets = read_assets(args.beancount_filename, args.currency, set(mapping), quantization)
 
     # For each of the assets, find and copy their corresponding details and fill
     # in the balance amounts.
