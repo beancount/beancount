6d9de5bd56599676d64e81f9cfc4692405656075
Martin Blais <blais@furius.ca>
2017-09-24 03:13:05 -0400

    Made the copmare_download_formats experimental script automatically fetch the index and all the documents it contains.

--- experiments/docs/convert/compare_download_formats.py
+++ experiments/docs/convert/compare_download_formats.py
@@ -22,8 +22,13 @@ import re
 import pickle
 import hashlib
 import shelve
+import urllib.parse
 from os import path
 
+from typing import List
+
+import bs4
+import apiclient.errors
 from apiclient import discovery
 import httplib2
 from oauth2client import service_account
@@ -37,14 +42,19 @@ def download_doc(files: object, docid: str,
     clean_name = re.sub('_-_', '-',
                         re.sub('_+', '_',
                                re.sub('[^A-Za-z0-9=-]', '_', name)))
-    filename = path.join(output_dir, '{}.{}'.format(clean_name, extension))
+    filename = path.join(output_dir, docid, '{}.{}'.format(clean_name, extension))
+    os.makedirs(path.dirname(filename), exist_ok=True)
     if path.exists(filename) and path.getsize(filename):
         logging.warning("File already present: %s; skipping", filename)
         return filename
-    with open(filename, 'wb') as outfile:
-        exported = files.export(fileId=docid,
-                                mimeType=mime_type).execute()
-        outfile.write(exported)
+    try:
+        with open(filename, 'wb') as outfile:
+            exported = files.export(fileId=docid,
+                                    mimeType=mime_type).execute()
+            outfile.write(exported)
+    except apiclient.errors.HttpError as exc:
+        logging.error("Skipping; Error downloading: %s", exc)
+        filename = None
     return filename
 
 
@@ -59,10 +69,28 @@ FORMATS = [
     ]
 
 
+INDEX_DOCID = '1RaondTJCS_IUPBHFNdT8oqFKJjVJDsfsn6JEjBG04eA'
+
+
+def get_docids_from_index(files: discovery.Resource):
+    """Given a files service, get the doc list of doc ids from the index page.
+    """
+    html = files.export(fileId=INDEX_DOCID, mimeType='text/html').execute()
+    doc = bs4.BeautifulSoup(html, 'lxml')
+    for a in doc.find_all('a'):
+        href = a['href']
+        #print(href)
+        match = re.search(r'document/d/(.*)/', href)
+        if not match:
+            continue
+        #print(match.group(1))
+        yield match.group(1)
+
+
 SERVICE_ACCOUNT_FILE = path.join(os.environ['HOME'],
                                  '.google-apis-service-account.json')
 
-def get_auth_via_service_account(scopes):
+def get_auth_via_service_account(scopes: List[str]):
     """Get an authenticated http object via a service account.
 
     Args:
@@ -82,7 +110,7 @@ def main():
     logging.basicConfig(level=logging.INFO, format='%(levelname)-8s: %(message)s')
     parser = argparse.ArgumentParser(description=__doc__.strip())
 
-    parser.add_argument('docid', action='store',
+    parser.add_argument('--docid', action='store',
                         default=None,
                         help="The document id of the doc to download")
 
@@ -96,24 +124,33 @@ def main():
     def get_service():
         scopes = ['https://www.googleapis.com/auth/drive']
         _, http = get_auth_via_service_account(scopes)
-        service = discovery.build('drive', 'v3', http=http)
+        service = discovery.build('drive', 'v3', http=http, cache_discovery=False)
         return service.files()
     files = get_service()
 
+    if args.docid:
+        docids = [args.docid]
+    else:
+        docids = sorted(set(get_docids_from_index(files)))
+
     # Download the docs.
-    os.makedirs(args.output, exist_ok=True)
-    for extension, mime_type, pandoc_format in FORMATS:
-        logging.info("Downloading to %s (%s)", extension, mime_type)
-        filename = download_doc(files, args.docid, extension, mime_type, args.output)
-        logging.info("File: %s", filename)
+    for docid in docids:
+        logging.info("-------------------- Document: %s", docid)
+        os.makedirs(args.output, exist_ok=True)
+        for extension, mime_type, pandoc_format in FORMATS:
+            logging.info("Downloading to %s (%s)", extension, mime_type)
+            filename = download_doc(files, docid, extension, mime_type, args.output)
+            logging.info("File: %s", filename)
 
-        if pandoc_format:
-            native_filename = path.join(args.output, "native-from-{}".format(extension))
-            logging.info("Calling pandoc for %s", native_filename)
-            subprocess.check_call([
-                'pandoc', '--from={}'.format(pandoc_format), '--to=native',
-                filename, '--output={}'.format(native_filename)])
-
+            if pandoc_format:
+                native_filename = filename + '.pandoc'
+                logging.info("Calling pandoc for %s", native_filename)
+                try:
+                    subprocess.check_call([
+                        'pandoc', '--from={}'.format(pandoc_format), '--to=native',
+                        filename, '--output={}'.format(native_filename)])
+                except subprocess.CalledProcessError as exc:
+                    logging.error("Skipping; error in Pandoc conversion: %s", exc)
     logging.info("Done.")
 
 
