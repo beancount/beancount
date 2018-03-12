fe5a6b6897bdb71fbdc789249a5a0b392abffcb2
Michael Droogleever <droogmic@yahoo.com>
2018-03-12 23:18:37 +0100

    Prepare for refactor
    turn ingest/importers/csv into a directory
    rename existing csv.py to simplecsv.py

---
+++ beancount/ingest/importers/csv/__init__.py
@@ -0,0 +1,7 @@
+"""
+CSV importer.
+"""
+__copyright__ = "Copyright (C) 2016 Martin Blais, 2018 Michael Droogleever"
+__license__ = "GNU GPLv2"
+
+from beancount.ingest.importers.csv.simplecsv import Col, Importer, normalize_config
