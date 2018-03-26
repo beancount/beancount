8b41d3a1146a14c5093d1f5d466d23c39d3cfbc3
Martin Blais <blais@furius.ca>
2018-03-26 17:28:15 -0400

    Fixed #245: Synchronized the definition of a valid account regular expression between the lexer and the later enhanced version (in Python) which checks the configured prefixes.

--- beancount/parser/grammar.py
+++ beancount/parser/grammar.py
@@ -96,7 +96,11 @@ def valid_account_regexp(options):
                                       'name_equity',
                                       'name_income',
                                       'name_expenses'))
-    return re.compile('({})(:[A-Z][A-Za-z0-9\-]*)*$'.format('|'.join(names)))
+    # Replace the first term of the account regular expression with the specific
+    # names allowed under the options configuration.
+    regexp = re.sub(r'\(.*?\)', '({})'.format('|'.join(names)),
+                    account.ACCOUNT_RE + '$', count=1)
+    return re.compile(regexp)
 
 
 # A temporary data structure used during parsing to hold and accumulate the
@@ -133,7 +137,7 @@ class Builder(lexer.LexBuilder):
         self.options['filename'] = filename
 
         # Make the account regexp more restrictive than the default: check
-        # types.
+        # types. Warning: This overrides the value in the base class.
         self.account_regexp = valid_account_regexp(self.options)
 
         # A display context builder.
