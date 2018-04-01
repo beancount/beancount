b4b739204e3065455a3df7083ae388915be13679
Martin Blais <blais@furius.ca>
2018-04-01 00:33:31 -0400

    Removed unused and invalid mutating method Position.set_units(). Amazing obsolete stuff like this still exists.

--- beancount/core/position.py
+++ beancount/core/position.py
@@ -257,16 +257,6 @@ class Position(_Position):
         # Note: We use Decimal() for efficiency.
         return Position(copy.copy(self.units), copy.copy(self.cost))
 
-    def set_units(self, units):
-        """Set the units. This is required to abstract over the old and the new position
-        object.
-
-        Args:
-          units: An instance of Amount.
-        """
-        assert isinstance(units, Amount)
-        self.units = units  # pylint: disable=assigning-non-slot
-
     def currency_pair(self):
         """Return the currency pair associated with this position.
 
