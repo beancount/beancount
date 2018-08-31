96eb2e418ed3cf146ad6396193a0f9ec902222d4
Martin Blais <blais@furius.ca>
2018-08-30 23:56:56 -0400

    Made assertions on entries off of the TestCase class, as free functions.

--- beancount/parser/cmptest.py
+++ beancount/parser/cmptest.py
@@ -7,6 +7,8 @@ import unittest
 import io
 import textwrap
 
+import pytest
+
 from beancount.parser import parser
 from beancount.parser import printer
 from beancount.parser import booking
@@ -52,87 +54,100 @@ def read_string_or_entries(entries_or_str):
 class TestCase(unittest.TestCase):
 
     def assertEqualEntries(self, expected_entries, actual_entries):
-        """Compare two lists of entries exactly and print missing entries verbosely if
-        they occur.
-
-        Args:
-          expected_entries: Either a list of directives or a string, in which case the
-            string is run through beancount.parser.parse_string() and the resulting
-            list is used.
-          actual_entries: Same treatment as expected_entries, the other list of
-            directives to compare to.
-        Raises:
-          AssertionError: If the exception fails.
-        """
-        expected_entries = read_string_or_entries(expected_entries)
-        actual_entries = read_string_or_entries(actual_entries)
-
-        same, expected_missing, actual_missing = compare.compare_entries(expected_entries,
-                                                                         actual_entries)
-        if not same:
-            assert expected_missing or actual_missing, "Missing is missing: {}, {}".format(
-                expected_missing, actual_missing)
-            oss = io.StringIO()
-            if expected_missing:
-                oss.write("Present in expected set and not in actual set:\n\n")
-                for entry in expected_missing:
-                    oss.write(printer.format_entry(entry))
-                    oss.write('\n')
-            if actual_missing:
-                oss.write("Present in actual set and not in expected set:\n\n")
-                for entry in actual_missing:
-                    oss.write(printer.format_entry(entry))
-                    oss.write('\n')
-            self.fail(oss.getvalue())
-
+        return assertEqualEntries(expected_entries, actual_entries, self.fail)
     def assertIncludesEntries(self, subset_entries, entries):
-        """Check that subset_entries is included in entries and print missing entries.
-
-        Args:
-          subset_entries: Either a list of directives or a string, in which case the
-            string is run through beancount.parser.parse_string() and the resulting
-            list is used.
-          entries: Same treatment as subset_entries, the other list of
-            directives to compare to.
-        Raises:
-          AssertionError: If the exception fails.
-        """
-        subset_entries = read_string_or_entries(subset_entries)
-        entries = read_string_or_entries(entries)
-
-        includes, missing = compare.includes_entries(subset_entries, entries)
-        if not includes:
-            assert missing, "Missing is empty: {}".format(missing)
-            oss = io.StringIO()
-            if missing:
-                oss.write("Missing from from expected set:\n\n")
-                for entry in missing:
-                    oss.write(printer.format_entry(entry))
-                    oss.write('\n')
-            self.fail(oss.getvalue())
-
+        return assertIncludesEntries(subset_entries, entries, self.fail)
     def assertExcludesEntries(self, subset_entries, entries):
-        """Check that subset_entries is not included in entries and print extra entries.
+        return assertExcludesEntries(subset_entries, entries, self.fail)
 
-        Args:
-          subset_entries: Either a list of directives or a string, in which case the
-            string is run through beancount.parser.parse_string() and the resulting
-            list is used.
-          entries: Same treatment as subset_entries, the other list of
-            directives to compare to.
-        Raises:
-          AssertionError: If the exception fails.
-        """
-        subset_entries = read_string_or_entries(subset_entries)
-        entries = read_string_or_entries(entries)
 
-        excludes, extra = compare.excludes_entries(subset_entries, entries)
-        if not excludes:
-            assert extra, "Extra is empty: {}".format(extra)
-            oss = io.StringIO()
-            if extra:
-                oss.write("Extra from from first/excluded set:\n\n")
-                for entry in extra:
-                    oss.write(printer.format_entry(entry))
-                    oss.write('\n')
-            self.fail(oss.getvalue())
+DEFAULT_FAILFUNC = pytest.fail
+
+
+def assertEqualEntries(expected_entries, actual_entries, failfunc=DEFAULT_FAILFUNC):
+    """Compare two lists of entries exactly and print missing entries verbosely if
+    they occur.
+
+    Args:
+      expected_entries: Either a list of directives or a string, in which case the
+        string is run through beancount.parser.parse_string() and the resulting
+        list is used.
+      actual_entries: Same treatment as expected_entries, the other list of
+        directives to compare to.
+    Raises:
+      AssertionError: If the exception fails.
+    """
+    expected_entries = read_string_or_entries(expected_entries)
+    actual_entries = read_string_or_entries(actual_entries)
+
+    same, expected_missing, actual_missing = compare.compare_entries(expected_entries,
+                                                                     actual_entries)
+    if not same:
+        assert expected_missing or actual_missing, "Missing is missing: {}, {}".format(
+            expected_missing, actual_missing)
+        oss = io.StringIO()
+        if expected_missing:
+            oss.write("Present in expected set and not in actual set:\n\n")
+            for entry in expected_missing:
+                oss.write(printer.format_entry(entry))
+                oss.write('\n')
+        if actual_missing:
+            oss.write("Present in actual set and not in expected set:\n\n")
+            for entry in actual_missing:
+                oss.write(printer.format_entry(entry))
+                oss.write('\n')
+        failfunc(oss.getvalue())
+
+
+def assertIncludesEntries(subset_entries, entries, failfunc=DEFAULT_FAILFUNC):
+    """Check that subset_entries is included in entries and print missing entries.
+
+    Args:
+      subset_entries: Either a list of directives or a string, in which case the
+        string is run through beancount.parser.parse_string() and the resulting
+        list is used.
+      entries: Same treatment as subset_entries, the other list of
+        directives to compare to.
+    Raises:
+      AssertionError: If the exception fails.
+    """
+    subset_entries = read_string_or_entries(subset_entries)
+    entries = read_string_or_entries(entries)
+
+    includes, missing = compare.includes_entries(subset_entries, entries)
+    if not includes:
+        assert missing, "Missing is empty: {}".format(missing)
+        oss = io.StringIO()
+        if missing:
+            oss.write("Missing from from expected set:\n\n")
+            for entry in missing:
+                oss.write(printer.format_entry(entry))
+                oss.write('\n')
+        failfunc(oss.getvalue())
+
+
+def assertExcludesEntries(subset_entries, entries, failfunc=DEFAULT_FAILFUNC):
+    """Check that subset_entries is not included in entries and print extra entries.
+
+    Args:
+      subset_entries: Either a list of directives or a string, in which case the
+        string is run through beancount.parser.parse_string() and the resulting
+        list is used.
+      entries: Same treatment as subset_entries, the other list of
+        directives to compare to.
+    Raises:
+      AssertionError: If the exception fails.
+    """
+    subset_entries = read_string_or_entries(subset_entries)
+    entries = read_string_or_entries(entries)
+
+    excludes, extra = compare.excludes_entries(subset_entries, entries)
+    if not excludes:
+        assert extra, "Extra is empty: {}".format(extra)
+        oss = io.StringIO()
+        if extra:
+            oss.write("Extra from from first/excluded set:\n\n")
+            for entry in extra:
+                oss.write(printer.format_entry(entry))
+                oss.write('\n')
+        failfunc(oss.getvalue())
