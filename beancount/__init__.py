b7c5dd67dee53442b4629830a22d51e2b90410ef
Martin Blais <blais@furius.ca>
2019-01-29 22:15:32 -0500

    (2.2.1-dev)

--- beancount/__init__.py
+++ beancount/__init__.py
@@ -13,7 +13,7 @@ if (sys.version_info.major, sys.version_info.minor) < (3, 3):
     raise ImportError("Python 3.3 or above is required")
 
 
-__version__ = '2.2.1'
+__version__ = '2.2.1-dev'
 
 
 # Remove annoying warnings in third-party modules.
