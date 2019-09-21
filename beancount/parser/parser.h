499627ea83cac3b9c3eea547bdd9b4cdaa772715
Daniele Nicolodi <daniele@grinta.net>
2019-09-20 21:19:26 -0600

    parser: Step 1 toward a reentrant parser
    Enable "%option reentrant" in lexer.l and "%option api.pure" and
    "%param {yyscan_t scanner}" in grammar.y to generate a reentrant
    parser. For now keep a global varibale pointing to the parser. Adapt
    the code to the changed function signatures and new constraints.

--- beancount/parser/parser.h
+++ beancount/parser/parser.h
@@ -4,12 +4,18 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
-
 extern PyObject* builder;
 extern PyObject* missing_obj;
 
-/* #define DO_TRACE_ERRORS   1 */
+/* This typedef is included in the Flex generated lexer.h header, but
+ * it is needed in the Bison generted header grammar.h that needs to
+ * be included before lexer.h. Repeat it here and make sure to include
+ * the headers in the right order. */
+typedef void* yyscan_t;
 
+#define YY_DECL int yylex (YYSTYPE * yylval_param, YYLTYPE * yylloc_param, yyscan_t yyscanner)
+
+/* #define DO_TRACE_ERRORS   1 */
 
 /* Error tracing (use for debugging error handling). */
 #ifdef DO_TRACE_ERRORS
