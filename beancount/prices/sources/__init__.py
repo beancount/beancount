9e26d21c1848e89c6f63b76b96141adddb110a0d
Martin Blais <blais@furius.ca>
2020-07-25 12:25:36 -0400

    Removed bean-price from 'beancount' repo. Moving to 'beanprice' repo.

--- beancount/prices/sources/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Implementation of various price extractors.
-
-This package is looked up by the driver script to figure out which extractor to
-use.
-"""
-__copyright__ = "Copyright (C) 2015-2017  Martin Blais"
-__license__ = "GNU GPLv2"
