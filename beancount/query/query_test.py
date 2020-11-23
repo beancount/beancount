2aac1d13946ba891191434042407f9c68a435842
Martin Blais <blais@furius.ca>
2020-11-23 00:49:20 -0500

    Changed query test not to depend on sharing example file, which in turns requires split_expenses, which in turn is going to be moved to experiments in v3.

--- beancount/query/query_test.py
+++ beancount/query/query_test.py
@@ -8,26 +8,28 @@ from beancount.query import query
 from beancount.utils import test_utils
 from beancount import loader
 
-class TestNumerifySimple(unittest.TestCase):
+
+class TestSimple(unittest.TestCase):
 
     def test_run_query(self):
         rootdir = test_utils.find_repository_root(__file__)
-        filename = path.join(rootdir, 'examples', 'sharing', 'cozumel2015.beancount')
-        entries, _, options_map = loader.load_file(filename)
+        filename = path.join(rootdir, 'examples', 'example.beancount')
+        entries, errors, options_map = loader.load_file(filename)
+        assert not errors
         sql_query = r"""
           SELECT
-            PARENT(account) AS account,
+            account,
             SUM(position) AS amount
-          WHERE account ~ 'Expenses.*\b{}'
+          WHERE account ~ 'Expenses:'
           GROUP BY 1
           ORDER BY 2 DESC
         """
         rtypes, rrows = query.run_query(entries, options_map,
                                         sql_query, 'Martin',
                                         numberify=True)
-        self.assertEqual(['account', 'amount (USD)', 'amount (MXN)'],
+        self.assertEqual(['account', 'amount (USD)', 'amount (IRAUSD)', 'amount (VACHR)'],
                          [rt[0] for rt in rtypes])
-        self.assertEqual(13, len(rrows))
+        self.assertEqual(len(rrows[0]), 4)
 
 
 if __name__ == '__main__':
