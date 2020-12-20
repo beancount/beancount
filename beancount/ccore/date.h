878577669774b7bc47ee37cab531f8bed7347d6f
Martin Blais <blais@furius.ca>
2020-12-20 17:23:55 -0500

    cparser: Moved DateToProto() to own library.

---
+++ beancount/ccore/date.h
@@ -0,0 +1,27 @@
+// Date-related utilities.
+//
+// Copyright (C) 2020  Martin Blais
+// License: "GNU GPLv2"
+
+#ifndef _BEANCOUNT_CCORE_DATE_H_
+#define _BEANCOUNT_CCORE_DATE_H_
+
+#include "beancount/ccore/date.pb.h"
+
+#include <cassert>
+
+#include "absl/time/civil_time.h"
+
+namespace beancount {
+
+// Convert a date object to an equivalent proto.
+inline void DateToProto(const absl::CivilDay& date, Date* proto) {
+  assert(proto != nullptr);
+  proto->set_year(date.year());
+  proto->set_month(date.month());
+  proto->set_day(date.day());
+}
+
+}  // namespace beancount
+
+#endif  // _BEANCOUNT_CCORE_DATE_H_
