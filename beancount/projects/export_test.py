2bb6ed8012e181c1fed67a18187d242eeb99dca9
Daniele Nicolodi <daniele@grinta.net>
2021-01-27 18:11:04 +0100

    projects/export: Use Click for command line arguments parsing
    Switching all shipped code to Click allows to uniform the different
    tools and will allow to remove some code that is better maintained in
    an external library.
    
    Fix a file descriptor double close() bug en passant.

--- beancount/projects/export_test.py
+++ beancount/projects/export_test.py
@@ -3,6 +3,7 @@ __license__ = "GNU GPLv2"
 
 from os import path
 import unittest
+import click.testing
 
 from beancount.utils import test_utils
 from beancount.projects import export
@@ -13,10 +14,10 @@ class TestExport(test_utils.TestCase):
     def test_export_basic(self):
         rootdir = test_utils.find_repository_root(__file__)
         example_beancount = path.join(rootdir, 'examples', 'example.beancount')
-        with test_utils.capture('stdout', 'stderr'):
-            result = test_utils.run_with_args(export.main,
-                                              [example_beancount, '-o-'])
-            self.assertEqual(0, result)
+
+        runner = click.testing.CliRunner()
+        result = runner.invoke(export.main, [example_beancount, '-o', '-'])
+        self.assertEqual(0, result.exit_code)
 
 
 if __name__ == '__main__':
