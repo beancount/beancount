80f7b1f350b4671ee5cf6012187c37b53199c193
Martin Blais <blais@furius.ca>
2021-02-02 00:51:03 -0500

    Reduced requirements for finding repository root.

--- beancount/utils/test_utils.py
+++ beancount/utils/test_utils.py
@@ -52,7 +52,7 @@ def find_repository_root(filename=None):
         return match.group(1)
 
     while not all(path.exists(path.join(filename, sigfile))
-                  for sigfile in ('PKG-INFO', 'COPYING', 'README.rst')):
+                  for sigfile in ('PKG-INFO', 'COPYING')):
         prev_filename = filename
         filename = path.dirname(filename)
         if prev_filename == filename:
