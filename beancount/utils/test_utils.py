0542b96237283e0a587e171078d9bd5d4252b9b4
blais <blais@furius.ca>
2022-06-25 01:25:35 -0400

    Enabled lint unused-private-member

--- beancount/utils/test_utils.py
+++ beancount/utils/test_utils.py
@@ -235,7 +235,7 @@ def docfile(function, **kwargs):
     @functools.wraps(function)
     def new_function(self):
         allowed = ('buffering', 'encoding', 'newline', 'dir', 'prefix', 'suffix')
-        if any([key not in allowed for key in kwargs]):
+        if any(key not in allowed for key in kwargs):
             raise ValueError("Invalid kwarg to docfile_extra")
         with tempfile.NamedTemporaryFile('w', **kwargs) as file:
             text = contents or function.__doc__
