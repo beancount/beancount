fa2c8e5d55efdb4a35b763cb40abc4b9b126c522
blais <blais@furius.ca>
2022-09-11 15:55:58 -0400

    Figured out proto caster for Decimal types.

--- experiments/v3/decimal/decimal_func.cc
+++ experiments/v3/decimal/decimal_func.cc
@@ -6,7 +6,11 @@
 #include "pybind11/pybind11.h"
 #include "decimal.hh"
 
-//,------------------------------------------------------------
+namespace py = pybind11;
+using std::cout;
+using std::endl;
+
+//-------------------------------------------------------------------------------
 // Copied from Python:Modules/_cdecimal/_decimal.c
 
 extern "C" {
@@ -19,21 +23,66 @@ typedef struct {
     mpd_t dec;
     mpd_uint_t data[_Py_DEC_MINALLOC];
 } PyDecObject;
+
+static py::object Decimal;
+
+#define MPD(v) (&((PyDecObject *)v)->dec)
+
 }
 
-//`------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+// Type caster
+
+namespace pybind11 { namespace detail {
+
+template <> struct type_caster<decimal::Decimal> {
+public:
+  PYBIND11_TYPE_CASTER(decimal::Decimal, const_name("Decimal"));
+
+  // Convert Python PyObject to a C++ Decimal instance.
+  // Return false upon failure.
+  bool load(handle src, bool apply_implicit) {
+    mpd_copy(value.get(), MPD(src.ptr()), decimal::context.get());
+    return true;
+  }
+
+  // Convert a C++ Decimal instance to a Python PyObject.
+  static handle cast(const decimal::Decimal& src,
+                     return_value_policy /* policy */,
+                     handle /* parent */) {
+    py::object obj = Decimal();
+    PyDecObject* dobj = reinterpret_cast<PyDecObject*>(obj.ptr());
+    mpd_copy(MPD(dobj), src.getconst(), decimal::context.get());
+    return obj;
+  }
+};
+
+}} // namespace pybind11::detail
+
+//------------------------------------------------------------------------------
+// Implementations.
 
 namespace beancount {
-namespace py = pybind11;
-using std::cout;
-using std::endl;
 
 void AcceptDecimal(const py::object* obj) {
-  // const PyDecObject* dobj = obj->cast<PyDecObject*>();
   const PyObject* pyobj = obj->ptr();
   const PyDecObject* dobj = reinterpret_cast<const PyDecObject*>(pyobj);
   decimal::Decimal dec(mpd_as_uint128_triple(&dobj->dec));
-  cout << "XXX " << dec << endl;
+  cout << dec << endl;
+}
+
+void AcceptDecimalCastPtr(const decimal::Decimal* dec) {
+  cout << *dec << endl;
+}
+
+void AcceptDecimalCastRef(const decimal::Decimal& dec) {
+  cout << dec << endl;
+}
+
+decimal::Decimal TimesTwo(const decimal::Decimal& dec) {
+  return dec * 2;
 }
 
 }  // namespace beancount
@@ -42,7 +91,16 @@ PYBIND11_MODULE(decimal_func, mod) {
   mod.doc() = "Module to test cross-language Decimal serialization.";
   using namespace beancount;
 
+  // Get the global type constructor.
+  Decimal = py::module_::import("decimal").attr("Decimal");
+
   // Export parsing function.
   mod.def("accept_decimal", &AcceptDecimal, "Read a Decimal instance from Python",
-          py::arg("number"));
+          py::arg("obj"));
+  mod.def("accept_decimal_cast_ptr", &AcceptDecimalCastPtr, "Read a Decimal instance from Python",
+          py::arg("dec"));
+  mod.def("accept_decimal_cast_ref", &AcceptDecimalCastRef, "Read a Decimal instance from Python",
+          py::arg("dec"));
+  mod.def("times_two", &TimesTwo, "Multiply by two",
+          py::arg("dec"));
 }
--- experiments/v3/decimal/decimal_func_test.py
+++ experiments/v3/decimal/decimal_func_test.py
@@ -12,6 +12,9 @@ class DecimalFuncTests(unittest.TestCase):
 
   def test_accept_decimal(self):
       mod.accept_decimal(Decimal("0.01"))
+      mod.accept_decimal_cast_ptr(Decimal("0.02"))
+      mod.accept_decimal_cast_ref(Decimal("0.03"))
+      print(mod.times_two(Decimal("0.08")))
 
 
 if __name__ == '__main__':
