9c8968f94e136e3f0671d27d7d3dcb530ce33c71
blais <blais@furius.ca>
2024-06-16 17:51:21 -0400

    Fixed bug in workflow install-from-sources-old-style

--- beancount/parser/version.py
+++ beancount/parser/version.py
@@ -5,6 +5,7 @@ __license__ = "GNU GPLv2"
 
 import datetime
 import re
+import platform
 
 import beancount
 from beancount.parser import _parser
@@ -22,20 +23,26 @@ def compute_version_string(version, changeset, timestamp):
     """
     # Shorten changeset.
     if changeset:
-        if re.match('hg:', changeset):
+        if re.match("hg:", changeset):
             changeset = changeset[:15]
-        elif re.match('git:', changeset):
+        elif re.match("git:", changeset):
             changeset = changeset[:12]
 
     # Convert timestamp to a date.
     date = None
     if timestamp > 0:
-        date = datetime.datetime.fromtimestamp(timestamp, datetime.UTC).date()
+        utc = (
+            datetime.UTC
+            if platform.python_version_tuple() >= ("3", "11")
+            else datetime.timezone.utc
+        )
+        date = datetime.datetime.fromtimestamp(timestamp, datetime.timezone.utc).date()
 
-    version = 'Beancount {}'.format(version)
+    version = "Beancount {}".format(version)
     if changeset or date:
-        version = '{} ({})'.format(
-            version, '; '.join(map(str, filter(None, [changeset, date]))))
+        version = "{} ({})".format(
+            version, "; ".join(map(str, filter(None, [changeset, date])))
+        )
 
     return version
 
@@ -43,4 +50,5 @@ def compute_version_string(version, changeset, timestamp):
 VERSION = compute_version_string(
     beancount.__version__,
     getattr(_parser, "__vc_changeset__", None),
-    getattr(_parser, "__vc_timestamp__", 0))
+    getattr(_parser, "__vc_timestamp__", 0),
+)
