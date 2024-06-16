17b60d28f743b7ff196a84a641782f24c1f33f7c
blais <blais@furius.ca>
2024-06-16 13:50:01 -0400

    Importing changes from v2 that weren't in master

--- beancount/parser/printer.py
+++ beancount/parser/printer.py
@@ -145,7 +145,10 @@ class EntryPrinter:
             return
         if prefix is None:
             prefix = self.prefix
-        for key, value in sorted(meta.items()):
+
+        # Note: meta.items() is assumed stable from 3.7 onwards; we're not sorting
+        # on purpose in order to keep the original insertion order in print.
+        for key, value in meta.items():
             if key not in self.META_IGNORE and not key.startswith('__'):
                 value_str = None
                 if isinstance(value, str):
