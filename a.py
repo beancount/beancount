22a38190702ffd78c00947e16236f5c47a1ca9d1
Trim21 <trim21.me@gmail.com>
2024-11-07 14:41:43 +0800

    format

--- a.py
+++ a.py
@@ -7,7 +7,7 @@ assert Booking.STRICT == Booking.STRICT, "a == a"
 assert Booking.STRICT == "STRICT", 'a == "a"'
 assert Booking.STRICT is Booking.STRICT, "a is a"
 
-file = (parse('''
+file = parse("""
 include "a.bean"
 option "title" "Ed’s Personal Ledger"
 
@@ -18,7 +18,7 @@ option "title" "Ed’s Personal Ledger"
 2014-05-05 txn "Cafe Mogador" "Lamb tagine with wine"
   Liabilities:CreditCard:CapitalOne         -37.45 USD
   Expenses:Restaurant
-'''))
+""")
 
 print(file.includes)  # ['a.bean']
 
