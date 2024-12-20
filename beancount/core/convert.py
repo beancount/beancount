1f6ef68155205bd30db818a11fcc3bd6648765df
memst <stankev.martynas@gmail.com>
2024-12-20 10:32:20 +0000

    Fix typo in docstring.

--- beancount/core/convert.py
+++ beancount/core/convert.py
@@ -112,7 +112,7 @@ def get_value(pos, price_map, date=None, output_date_prices=None):
     Note that if the position is not held at cost, this does not convert
     anything, even if a price is available in the 'price_map'. We don't specify
     a target currency here. If you're attempting to make such a conversion, see
-    ``convert_*()`` functions below. However, is the object is a posting and it
+    ``convert_*()`` functions below. However, if the object is a posting and it
     has a price, we will use that price to infer the target currency and those
     will be converted.
 
