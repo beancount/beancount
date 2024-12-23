07e1800ff7fb89743d7a3311f8c5ff5fc9027232
Trim21 <trim21.me@gmail.com>
2024-12-23 21:51:55 +0800

    refactor: remove unused old code

--- beancount/utils/defdict_test.py
+++ beancount/utils/defdict_test.py
@@ -2,21 +2,8 @@ __copyright__ = "Copyright (C) 2016  Martin Blais"
 __license__ = "GNU GPLv2"
 import pickle
 import unittest
-from unittest import mock
 
 from beancount.utils import defdict
-
-
-class TestDefDictWithKey(unittest.TestCase):
-    def test_defdict_with_key(self):
-        factory = mock.MagicMock()
-        testdict = defdict.DefaultDictWithKey(factory)
-
-        testdict["a"]
-        testdict["b"]
-        self.assertEqual(2, len(factory.mock_calls))
-        self.assertEqual(("a",), factory.mock_calls[0][1])
-        self.assertEqual(("b",), factory.mock_calls[1][1])
 
 
 class TestImmutableDictWithDefault(unittest.TestCase):
