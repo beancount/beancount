311f4ba3c3c221d79889f2dbb53afa0a94237163
blais <blais@furius.ca>
2025-05-29 15:32:30 -0400

    Refactoring of the common bits of the currency contexts.
    This is a bit of ugly concrete inheritance but I'm willing to accept it because
    it's all in one file and reduces the amount of code and really highlights the
    common parts.

--- beancount/core/display_context.py
+++ beancount/core/display_context.py
@@ -92,23 +92,18 @@ class Align(enum.Enum):
     RIGHT = 3
 
 
-class _CurrencyContext:
-    """Precision data for a single currency from a population of numbers.
-
-    This object accumulates aggregate precision data about numbers that is then
-    used by the DisplayContext to manufacture appropriate Formatter objects.
+class _ContextBase:
+    """Base class for currency contexts.
 
     Attributes:
       has_sign: A boolean, true if at least one of the numbers has a negative or
         explicit positive sign.
       integer_max: The maximum number of digits for the integer part.
-      fractional_dist: A frequency distribution of fractionals seen in the input file.
     """
 
     def __init__(self) -> None:
         self.has_sign = False
         self.integer_max = 1
-        self.fractional_dist = distribution.Distribution()
 
     def __str__(self) -> str:
         fmt = (
@@ -116,7 +111,6 @@ class _CurrencyContext:
             "fractional_common={:<2}  fractional_max={:<2}  "
             '"{}" "{}"'
         )
-        dist = self.fractional_dist
 
         example = ""
         if self.has_sign:
@@ -140,8 +134,8 @@ class _CurrencyContext:
         return fmt.format(
             int(self.has_sign),
             self.integer_max,
-            "_" if dist.empty() else dist.mode(),
-            "_" if dist.empty() else dist.max(),
+            self.get_fractional_digits_common(),
+            self.get_fractional_digits_max(),
             example_common,
             example_max,
         )
@@ -159,17 +153,46 @@ class _CurrencyContext:
         if num_tuple.sign:
             self.has_sign = True
 
-        # Update the precision.
-        self.fractional_dist.update(-num_tuple.exponent)  # type: ignore[operator]
-
         # Update the maximum number of integral digits.
         integer_digits = len(num_tuple.digits) + num_tuple.exponent  # type: ignore[operator]
         self.integer_max = max(self.integer_max, integer_digits)
 
     def update_from(self, other: _CurrencyContext) -> None:
         self.has_sign = self.has_sign or other.has_sign
-        self.fractional_dist.update_from(other.fractional_dist)
         self.integer_max = max(self.integer_max, other.integer_max)
+
+
+class _CurrencyContext(_ContextBase):
+    """Precision data for a single currency from a population of numbers.
+
+    This object accumulates aggregate precision data about numbers that is then
+    used by the DisplayContext to manufacture appropriate Formatter objects.
+
+    Attributes:
+      fractional_dist: A frequency distribution of fractionals seen in the input file.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.fractional_dist = distribution.Distribution()
+
+    def get_fractional_digits_common(self):
+        dist = self.fractional_dist
+        return "_" if dist.empty() else dist.mode()
+
+    def get_fractional_digits_max(self):
+        dist = self.fractional_dist
+        return "_" if dist.empty() else dist.max()
+
+    def update(self, number: Decimal) -> None:
+        super().update(number)
+        # Update the precision.
+        num_tuple = number.as_tuple()
+        self.fractional_dist.update(-num_tuple.exponent)  # type: ignore[operator]
+
+    def update_from(self, other: _CurrencyContext) -> None:
+        super().update_from(other)
+        self.fractional_dist.update_from(other.fractional_dist)
 
     def get_fractional(self, precision: Precision) -> int | None:
         """
@@ -186,7 +209,7 @@ class _CurrencyContext:
             raise ValueError("Unknown precision: {}".format(precision))
 
 
-class _FixedPrecisionContext:
+class _FixedPrecisionContext(_ContextBase):
     """Fixed precision data for a single currency. Sign and max integer are still learned.
 
     This object stores fixed precision data about numbers for a single currency.
@@ -194,72 +217,18 @@ class _FixedPrecisionContext:
     objects.
 
     Attributes:
-      has_sign: A boolean, true if at least one of the numbers has a negative or
-        explicit positive sign.
-      integer_max: The maximum number of digits for the integer part.
       fractional_digits: An integer, the precision for both MOST_COMMON and MAXIMUM.
     """
 
     def __init__(self, fractional_digits: int) -> None:
-        self.has_sign = False
-        self.integer_max = 1
+        super().__init__()
         self.fractional_digits: int = fractional_digits
 
-    def __str__(self) -> str:
-        fmt = (
-            "sign={:<2}  integer_max={:<2}  "
-            "fractional_common={:<2}  fractional_max={:<2}  "
-            '"{}" "{}"'
-        )
+    def get_fractional_digits_common(self):
+        return self.fractional_digits
 
-        example = ""
-        if self.has_sign:
-            example += "-"
-        example += "0" * self.integer_max
-
-        example_common = example
-        fractional_common = self.get_fractional(Precision.MOST_COMMON)
-        if fractional_common is None:
-            example_common = example + ".*"
-        elif fractional_common > 0:
-            example_common = example + "." + ("0" * fractional_common)
-
-        example_max = example
-        fractional_max = self.get_fractional(Precision.MAXIMUM)
-        if fractional_max is None:
-            example_max = example + ".*"
-        elif fractional_max > 0:
-            example_max = example + "." + ("0" * fractional_max)
-
-        return fmt.format(
-            int(self.has_sign),
-            self.integer_max,
-            self.fractional_digits,
-            self.fractional_digits,
-            example_common,
-            example_max,
-        )
-
-    def update(self, number: Decimal) -> None:
-        # Note: Please do care for the performance of this routine. This is run
-        # on a large set of numbers, possibly even during parsing. Consider
-        # reimplementing this in C, after profiling.
-
-        if number is None:
-            return
-
-        # Update the signs.
-        num_tuple = number.as_tuple()
-        if num_tuple.sign:
-            self.has_sign = True
-
-        # Update the maximum number of integral digits.
-        integer_digits = len(num_tuple.digits) + num_tuple.exponent  # type: ignore[operator]
-        self.integer_max = max(self.integer_max, integer_digits)
-
-    def update_from(self, other: _CurrencyContext) -> None:
-        self.has_sign = self.has_sign or other.has_sign
-        self.integer_max = max(self.integer_max, other.integer_max)
+    def get_fractional_digits_max(self):
+        return self.fractional_digits
 
     def get_fractional(self, precision: Precision) -> int | None:
         return self.fractional_digits
