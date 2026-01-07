bd283446dc28062a8da2c25d2f8f7ed43993c583
trim21 <i@trim21.me>
2026-01-07 15:55:02 +0800

    fix lint

--- beancount/parser/booking_method.py
+++ beancount/parser/booking_method.py
@@ -5,7 +5,7 @@ This code is used by the full booking algorithm.
 __copyright__ = "Copyright (C) 2015-2017, 2019-2022, 2024  Martin Blais"
 __license__ = "GNU GPLv2"
 
-import typing
+from decimal import Decimal  # noqa: TC003
 from typing import NamedTuple
 
 from beancount.core import convert
@@ -17,11 +17,7 @@ from beancount.core.data import Booking
 from beancount.core.data import Directive
 from beancount.core.data import Meta
 from beancount.core.number import ZERO
-
-if typing.TYPE_CHECKING:
-    from decimal import Decimal
-
-    from beancount.core.position import Cost
+from beancount.core.position import Cost  # noqa: TC001
 
 
 class AmbiguousMatchError(NamedTuple):
