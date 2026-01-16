3753c6f1daa0a4215efd570711b687a91629280d
trim21 <i@trim21.me>
2026-01-16 16:14:17 +0800

    fix for wasm32

--- crates/beancount-parser/src/lib.rs
+++ crates/beancount-parser/src/lib.rs
@@ -38,14 +38,3 @@ pub fn parse_str<'a>(
 
   Ok(directives)
 }
-
-/// Parse file into typed directives.
-///
-/// This is primarily intended for tests and debugging.
-pub fn parse_directives_with_meta<'a>(
-  root: tree_sitter::Node,
-  source: &'a str,
-  filename: String,
-) -> Result<Vec<ast::Directive<'a>>, parse::ParseError> {
-  parse::parse_directives(root, source, filename)
-}
