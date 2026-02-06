e084af487707717c638aafebdc28ef6c4c7ba378
trim21 <i@trim21.me>
2026-02-06 22:49:16 +0800

    fix __version__

--- beancount/__init__.py
+++ beancount/__init__.py
@@ -3,14 +3,4 @@
 __copyright__ = "Copyright (C) 2013-2014, 2016-2025  Martin Blais"
 __license__ = "GNU GPLv2"
 
-
-# Check the version requirements.
-import sys
-
-if (sys.version_info.major, sys.version_info.minor) < (3, 7):
-    raise ImportError("Python 3.7 or above is required")
-
-
-# Expose the public API.
-del sys
 from .api import *  # noqa: E402, F403
